3496cad90d6225bca83351c41a4a150fc42eb544
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-02-06 14:10:44 -0800

    Now create also a library which can be linked against from C/C++, which contains ModelInterface, FakePlugin and FixedPointSource

--- setup.py
+++ setup.py
@@ -8,38 +8,81 @@ import os
 boost_root = os.environ.get("BOOSTROOT")
 
 if(boost_root):
+  #The user want to override pre-defined location of boost
+  
   print("\n\n **** Using boost.python from the env. variable $BOOSTROOT (%s)" %(boost_root))
+  
   include_dirs = [ os.path.join(boost_root,'include')]
   library_dirs = [ os.path.join(boost_root,'lib') ]
+  
   print("     Include dir: %s" %(include_dirs))
   print("     Library dir: %s" %(library_dirs))
+
 else:
+
   include_dirs = []
   library_dirs = []
+
 pass
  
-setup(name="threeML",
+setup(
+    
+    name="threeML",
+    
     packages = ['threeML',
                 'threeML/bayesian',
                 'threeML/minimizer',
                 'threeML/models',
                 'threeML/plugins'],
+    
     version = 'v0.0.5',
+    
     description = "The Multi-Mission Maximum Likelihood framework",
+    
     author = 'Giacomo Vianello',
+    
     author_email = 'giacomo.vianello@gmail.com',
+    
     url = 'https://github.com/giacomov/3ML',
+    
     download_url = 'https://github.com/giacomov/3ML/archive/v0.0.5',
+    
     keywords = ['Likelihood','Multi-mission','3ML','HAWC','Fermi','joint','fit'],
+    
     classifiers = [],
+    
     ext_modules=[
-        Extension("threeML.ModelInterface", ["threeML/models/ModelInterface.cxx",
-                                     "threeML/models/ModelInterface_boost.cxx"],
+        
+        Extension("threeML.pyModelInterface", 
+                  
+                  ["threeML/models/ModelInterface_boost.cxx"],
+        
+        
+        
         libraries = ["boost_python"],
+        
         include_dirs=include_dirs,
-        library_dirs=library_dirs)
+        
+        library_dirs=library_dirs),
+        
+        Extension("threeML.libModelInterface", 
+                  
+                  ["threeML/models/ModelInterface.cxx",
+                   "threeML/models/FixedPointSource.cxx"],
+        
+        
+        
+        libraries = ["boost_python"],
+        
+        include_dirs=include_dirs,
+        
+        library_dirs=library_dirs),
     ],
-    headers=["threeML/models/ModelInterface.h"],
+    
+    
+    headers=["threeML/models/ModelInterface.h",
+             "threeML/models/FixedPointSource.h"],
+    
     install_requires=[
           'numpy',
           'scipy',
