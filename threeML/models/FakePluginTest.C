d277ac9958707d45a572a1a8cbd869c80528abdf
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-02-07 00:08:41 -0800

    An example on how to use a plugin with a FixedPointSource ModelInterface

---
+++ threeML/models/FakePluginTest.C
@@ -0,0 +1,29 @@
+#include <threeML/FixedPointSource.h>
+#include <threeML/FakePlugin.h>
+#include <iostream>
+#include <math.h>
+
+int main(int argc, char **argv) {
+  //Since I don't specify the parameters for the FixedPointSource
+  //the values for the Crab will be used (see FixedPointSource.h)
+  threeML::FixedPointSource src;
+  
+  //Print info on the source
+  src.describe();
+  
+  //A fake plugin which just showcase how to get and use the
+  //ModelInterface instance
+  //(it will also print some info during the constructor)
+  threeML::FakePlugin fp(&src);
+  
+  //Create a certain number of energy bins
+  //to be used in the flux calculation
+  int n = 20;
+  fp.createEnergies(n);
+  
+  //This will compute and print the flux
+  //from the FixedPointSource at the energies
+  //defined above
+  fp.go();
+  
+}
