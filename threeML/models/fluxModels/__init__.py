43c2333008b4c5ac4c9c42ecf42296b6619f5986
J. Michael Burgess <jmichaelburgess@gmail.com>
2015-02-26 15:57:05 -0800

    added models

---
+++ threeML/models/fluxModels/__init__.py
@@ -0,0 +1,6 @@
+from band import Band
+from powerlaw import PowerLaw
+from logporabola import LogParabola
+from madness import Madness
+from manybrokenpowerlaws import ManyBrokenPowerlaws
+from manylogporabolas import ManyLogParabolas
