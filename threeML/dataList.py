0534cb2563cd43b669643fb505f16dfce88d014b
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-03-16 21:41:58 -0700

    Now DataList is a child class of collections.OrderedDict

--- threeML/dataList.py
+++ threeML/dataList.py
@@ -1,19 +1,32 @@
+#Author: G.Vianello (giacomov@stanford.edu)
+
 import collections
 
-class dataList(object):
-  def __init__(self,*datasets):
+class DataList(collections.OrderedDict):
+  '''
+  A container for datasets. Can be accessed as a dictionary,
+  with the [key] operator.
+  '''
+  def __init__(self, *datasets):
     
     #Enforce the uniqueness of the names for the datasets
     names                     = map(lambda x:x.getName(),datasets)
-    if(len(names)!=len(set(names))):
-      raise RuntimeError("Duplicated names for datasets. You have to use a unique name for each dataset.")
-    pass
-
-    #Save in a ordered dictionary (which keeps the order of the elements)
-    self.datasets             = collections.OrderedDict()  
+    
+    #sets contains by definition only unique elements
+    uniqueNames               = set(names)
+    
+    if( len(names) != len(uniqueNames) ):
+    
+      raise RuntimeError("Duplicated names for datasets. You have to use"+
+                         " a unique name for each dataset.")
+    
+    #Build the parent class
+    super(DataList, self).__init__()
+    
+    
+    #Store the datasets
+    
     for ds in datasets:
-      self.datasets[ds.getName()] = ds
-    pass        
-  pass
-  
+      
+      self[ds.getName()]           = ds       
 pass
