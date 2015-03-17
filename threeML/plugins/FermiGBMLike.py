57a7b145e060bbddb4decdb39263403b02efb95e
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-03-16 21:46:33 -0700

    Fully functional GBM plugin, compatible with the new interface

--- threeML/plugins/FermiGBMLike.py
+++ threeML/plugins/FermiGBMLike.py
@@ -1,4 +1,4 @@
-import pyfits
+import astropy.io.fits as pyfits
 import xml.etree.ElementTree as ET
 import os
 from threeML.plugins.gammaln import logfactorial
@@ -6,6 +6,8 @@ import numpy
 from threeML.plugins.ogip import OGIPPHA
 from threeML.pluginPrototype import pluginPrototype
 import scipy.integrate
+
+import warnings
 
 __instrument_name = "Fermi GBM (all detectors)"
 
@@ -17,18 +19,26 @@ class FermiGBMLike(pluginPrototype):
     FermiGBMLike("GBM","spectrum.pha{2}","bkgfile.bkg{2}","rspfile.rsp{2}")
     to load the second spectrum, second background spectrum and second response.
     '''
+    
+    #Check that all file exists
     notExistant               = []
+    
     if(not os.path.exists(phafile.split("{")[0])):
       notExistant.append(phafile.split("{")[0])
+    
     if(not os.path.exists(bkgfile.split("{")[0])):
       notExistant.append(bkgfile.split("{")[0])
+    
     if(not os.path.exists(rspfile.split("{")[0])):
       notExistant.append(rspfile.split("{")[0])
+    
     if(len(notExistant)>0):
+      
       for nt in notExistant:
         print("File %s does not exists!" %(nt))
+      
       raise IOError("One or more input file do not exist!")
-    pass
+    
     
     self.phafile              = OGIPPHA(phafile,filetype='observed')
     self.exposure             = self.phafile.getExposure()
@@ -37,24 +47,32 @@ class FermiGBMLike(pluginPrototype):
     
     #Start with an empty mask (the user will overwrite it using the
     #setActiveMeasurement method)
-    self.mask                 = numpy.asarray(numpy.ones(self.phafile.getRates().shape),numpy.bool)
+    self.mask                 = numpy.asarray(
+                                    numpy.ones(self.phafile.getRates().shape),
+                                    numpy.bool)
     
     #Get the counts for this spectrum
-    self.counts               = self.phafile.getRates()[self.mask]*self.exposure
-    self.bkgCounts            = self.bkgfile.getRates()[self.mask]*self.exposure
+    self.counts               = ( self.phafile.getRates()[self.mask]
+                                  * self.exposure )
+    
+    self.bkgCounts            = ( self.bkgfile.getRates()[self.mask]
+                                  * self.exposure )
     
     self.name                 = name
   pass
     
   def setActiveMeasurements(self,*args):
-    '''Set the measurements to be used during the analysis. Use as many ranges as you need,
+    '''Set the measurements to be used during the analysis. 
+    Use as many ranges as you need,
     specified as 'emin-emax'. Energies are in keV. Example:
     
     setActiveMeasurements('10-12.5','56.0-100.0')
     
-    which will set the energy range 10-12.5 keV and 56-100 keV to be used in the analysis'''
+    which will set the energy range 10-12.5 keV and 56-100 keV to be 
+    used in the analysis'''
     
-    #To implelemnt this we will use an array of boolean index, which will filter
+    #To implelemnt this we will use an array of boolean index, 
+    #which will filter
     #out the non-used channels during the logLike
     
     #Now build the mask: values for which the mask is 0 will be masked
@@ -72,7 +90,9 @@ class FermiGBMLike(pluginPrototype):
     self.counts               = self.phafile.getRates()[self.mask]*self.exposure
     self.bkgCounts            = self.bkgfile.getRates()[self.mask]*self.exposure
     
-    print("Now using %s channels out of %s" %(numpy.sum(self.mask),self.phafile.getRates().shape[0]))
+    print("Now using %s channels out of %s" %( numpy.sum(self.mask),
+                                               self.phafile.getRates().shape[0]
+                                              ) )
   pass
   
   def getName(self):
@@ -82,50 +102,94 @@ class FermiGBMLike(pluginPrototype):
     return self.name
   pass
   
-  def setModel(self,ModelManagerInstance):
+  def setModel(self,likelihoodModel):
     '''
-    Set the model to be used in the joint minimization. Must be a ModelManager instance.
+    Set the model to be used in the joint minimization.
     '''
-    self.modelManager         = ModelManagerInstance
-    self.response.setFunction(self.modelManager.spectralModel,self.modelManager.spectralModel.integral)
+    self.likelihoodModel         = likelihoodModel
+    
+    nPointSources                = self.likelihoodModel.getNumberOfPointSources()
+    
+    #This is a wrapper which iterates over all the point sources and get
+    #the fluxes
+    #We assume there are no extended sources, since the GBM cannot handle them
+    
+    def diffFlux(energies):
+      
+      fluxes                     = self.likelihoodModel.getPointSourceFluxes(0,energies)
+      
+      #If we have only one point source, this will never be executed
+      for i in range(1, nPointSources):
+        fluxes                  += self.likelihoodModel.getPointSourceFluxes(i,energies)
+      
+      return fluxes
+    
+    self.diffFlux                = diffFlux
+    
+    #The following integrates the diffFlux function using Simpson's rule
+    #This assume that the intervals e1,e2 are all small, which is guaranteed
+    #for any reasonable response matrix, given that e1 and e2 are Monte-Carlo
+    #energies. It also assumes that the function is smooth in the interval
+    #e1 - e2 and twice-differentiable, again reasonable on small intervals for
+    #decent models. It might fail for models with too sharp features, smaller
+    #than the size of the monte carlo interval.
+    
+    def integral(e1,e2):
+      
+      #Simpson's rule
+      
+      return (e2 - e1) / 6.0 * ( self.diffFlux(e1) 
+                                 + 4 * self.diffFlux( (e1+e2) / 2.0 )
+                                 + self.diffFlux(e2) )
+    
+    self.response.setFunction( diffFlux,
+                               integral)
   pass
 
   def innerFit(self):
-    '''
-    This is used for the profile likelihood. Keeping fixed all parameters in the
-    modelManager, this method minimize the logLike over the remaining nuisance
-    parameters, i.e., the parameters belonging only to the model for this
-    particular detector
-    '''
-    #There are no nuisance parameters here
-    return self.getLogLike()
-  pass
+  
+    #There are no nuisance parameters here (at least for now)
+    
+    pass
   
   def getFoldedModel(self):
-    #Get the folded model for this spectrum (this is the rate predicted, in cts/s)    
+    
+    #Get the folded model for this spectrum 
+    #(this is the rate predicted, in cts/s)    
+    
     return self.response.convolve()[self.mask]
-  pass
   
   def getModelAndData(self):
-    e1,e2                     = (self.response.ebounds[:,0],self.response.ebounds[:,1])
-    return self.response.convolve()[self.mask]*self.exposure+self.bkgCounts,e1[self.mask],e2[self.mask],self.counts
+    
+    e1,e2                     = (self.response.ebounds[:,0],
+                                 self.response.ebounds[:,1])
+    
+    return ( self.response.convolve()[self.mask] * self.exposure 
+           + self.bkgCounts, 
+             e1[self.mask],
+             e2[self.mask],
+             self.counts )
   
   def getLogLike(self):
     '''
     Return the value of the log-likelihood with the current values for the
     parameters
     '''
-    #Get the folded model for this spectrum (this is the rate predicted, in cts/s)    
+    
+    #Get the folded model for this spectrum (this is the rate predicted, 
+    #in cts/s)    
     folded                    = self.getFoldedModel()
     
-    #Model is folded+background (i.e., we assume negligible errors on the background)
-    modelCounts               = folded*self.exposure + self.bkgCounts
+    #Model is folded+background (i.e., we assume negligible errors on the 
+    #background)
+    modelCounts               = folded * self.exposure + self.bkgCounts
     
-    logLike                   = numpy.sum(-modelCounts+self.counts*numpy.log(modelCounts)-logfactorial(self.counts))
+    logLike                   = numpy.sum(- modelCounts 
+                                          + self.counts * numpy.log(modelCounts)
+                                          - logfactorial(self.counts) )
     
     return logLike
-  pass
-  
+      
   def getNuisanceParameters(self):
     '''
     Return a list of nuisance parameter names. Return an empty list if there
@@ -139,73 +203,126 @@ class Response(object):
   def __init__(self,rspfile):
     
     rspNumber                   = 1
+    
+    #Check if we are dealing with a .rsp2 file (containing more than
+    #one response). This is checked by looking for the syntax 
+    # [responseFile]{[responseNumber]}
+    
     if('{' in rspfile):
+    
       tokens                    = rspfile.split("{")
       rspfile                   = tokens[0]
       rspNumber                 = int(tokens[-1].split('}')[0].replace(" ",""))
-    pass
+    
     
     #Read the response
     with pyfits.open(rspfile) as f:
+      
       try:
+        
+        #GBM typical response
         data                    = f['MATRIX',rspNumber].data
+      
       except:
+        #Other detectors might use the SPECRESP MATRIX name instead
+        #Note that here we are not catching any exception, because
+        #we have to fail if we cannot read the matrix
+        
         data                    = f['SPECRESP MATRIX',rspNumber].data
-      pass    
+      
+      #Sometimes .rsp files contains a weird format featuring variable-length
+      #arrays. Historically those confuse pyfits quite a lot, so we ensure 
+      #to transform them into standard numpy matrices to avoid issues
+      
       self.matrix               = variableToMatrix(data.field('MATRIX'))
-      self.ebounds              = numpy.vstack([f['EBOUNDS'].data.field("E_MIN"),f['EBOUNDS'].data.field("E_MAX")]).T
-      self.mc_channels          = numpy.vstack([data.field("ENERG_LO"),data.field("ENERG_HI")]).T
-    pass
+      
+      self.ebounds              = numpy.vstack([ f['EBOUNDS'].data.field("E_MIN"),
+                                                 f['EBOUNDS'].data.field("E_MAX") ]).T
+      
+      self.mc_channels          = numpy.vstack([ data.field("ENERG_LO"),
+                                                 data.field("ENERG_HI") ]).T
     
-  pass
-  
-  def setFunction(self,differentialFunction,integralFunction=None):
+  def setFunction(self, differentialFunction, integralFunction=None):
+    '''
+    Set the function to be used for the convolution
+    '''
+    
     self.differentialFunction   = differentialFunction
+    
     if(integralFunction==None):
+      
+      #This should never happen in 3ML, but we keep this functionality for
+      #other uses. Print a warning anyway
+      
+      warnings.warn("Using the slow numerical integration in the GBM plugin!", 
+                     RuntimeWarning)
+      
       def integral(x,y):
         return scipy.integrate.quad(self.differentialFunction,x,y)[0]
+      
+      #NB: vectorize is super slow!
       self.integralFunction     = numpy.vectorize(integral,otypes=[numpy.float])
+    
     else:
+      
       self.integralFunction     = integralFunction
-    pass
-  pass
+    
   
+    
   def convolve(self):
-    trueFluxes                  = self.integralFunction(self.mc_channels[:,0],self.mc_channels[:,1])
+    
+    trueFluxes                  = self.integralFunction( self.mc_channels[:,0],
+                                                         self.mc_channels[:,1] )
   
-    foldedCounts                = numpy.dot(trueFluxes,self.matrix.T)
+    foldedCounts                = numpy.dot( trueFluxes, self.matrix.T )
+    
     return foldedCounts
-  pass
   
   def getCountsVector(self,e1,e2):
-    trueFluxes                  = self.integralFunction(self.mc_channels[:,0],self.mc_channels[:,1])
     
-  pass
+    trueFluxes                  = self.integralFunction(self.mc_channels[:,0],self.mc_channels[:,1])
   
   def energyToChannel(self,energy):
-    '''Finds the channel containing the provided energy. NOTE: returns the channel index (starting at zero),
+    
+    '''Finds the channel containing the provided energy. 
+    NOTE: returns the channel index (starting at zero),
     not the channel number (likely starting from 1)'''
     
     #Get the index of the first ebounds upper bound larger than energy
+    
     try:
-      idx                         = next(idx for idx, value in enumerate(self.ebounds[:,1]) if value >= energy)
+    
+      idx                         = next( idx for idx,
+                                          value in enumerate(self.ebounds[:,1]) 
+                                          if value >= energy )
+    
     except StopIteration:
+      
       #No values above the given energy, return the last channel
       return self.ebounds[:,1].shape[0]
+    
     return idx
-  pass
-pass
+  
 
 def variableToMatrix(variableLengthMatrix):
-  '''This take a variable length array and return it in a properly formed constant length array, to avoid some pyfits obscure bugs'''
+  '''This take a variable length array and return it in a 
+  properly formed constant length array, to avoid some pyfits obscure bugs'''
+  
   nrows                          = len(variableLengthMatrix)
   ncolumns                       = max([len(elem) for elem in variableLengthMatrix])
   matrix                         = numpy.zeros([ncolumns,nrows])
+  
   for i in range(nrows):
     for j in range(ncolumns):
+      
       try:
+      
         matrix[j,i]                = variableLengthMatrix[i][j]
+      
       except:
+        
+        #This happens when the i,j element does not exist, which is not an error
+        #We will just leave it to zero
         pass
+        
   return matrix
-pass
