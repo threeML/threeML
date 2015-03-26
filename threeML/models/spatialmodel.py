5a73164956bfd0c1de2aa96d887cdcdc98f90c64
tibaldo <ltibaldo@slac.stanford.edu>
2015-03-25 18:03:49 -0700

    added general class for spatial models

---
+++ threeML/models/spatialmodel.py
@@ -0,0 +1,20 @@
+import collections
+from Parameter import Parameter, SpatialParameter
+from IPython.display import display, Latex, HTML
+import abc
+
+
+
+class SpatiallModel(object):
+    __metaclass__           = abc.ABCMeta # Make the user code certain
+    
+
+    def __init__(self):
+
+        import collections        
+        self.setup()
+
+
+
+
+
