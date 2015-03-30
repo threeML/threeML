df647e3ddbc91d1f95d2b5e2085da9cd299475a7
tibaldo <ltibaldo@slac.stanford.edu>
2015-03-29 21:35:24 -0700

    fixed bugs in gaussian functions
    simple gaussian expressed in terms of angular separation to ensure
    maximum numerical precision in integral ==1 regardless of position;
    added rad to deg factor to ensure proper normalization; added missing
    ^2 in multivariate gaussain

--- threeML/models/spatialModels/gaussian.py
+++ threeML/models/spatialModels/gaussian.py
@@ -12,7 +12,7 @@ class Gaussian(SpatialModel):
 
     def setup(self):
         self.functionName        = "Gaussian"
-        self.formula             = r'$f(\vec{x}) = \frac{1}{2\pi \sigma^2} \, {\rm exp}\left(-\frac{(\vec{x}-\vec{x}_0)^2}{2\sigma^2} \right) \\ \vec{x}_0 = ({\rm RA}_0,{\rm Dec}_0)$'
+        self.formula             = r'$f({\rm RA, Dec}) = \left(\frac{180^\circ}{\pi}\right)^2 \frac{1}{2\pi \sigma^2} \, {\rm exp}\left(-\frac{{\rm angsep}^2 ({\rm RA, Dec, RA_0, Dec_0})}{2\sigma^2} \right) $'
         self.parameters          = collections.OrderedDict()
         self.parameters['RA0']     = Parameter('RA0',1.,0,360,0.1,fixed=False,nuisance=False,dataset=None)
         self.parameters['Dec0']     = Parameter('Dec0',1.,-90,90,0.1,fixed=False,nuisance=False,dataset=None)
@@ -27,14 +27,15 @@ class Gaussian(SpatialModel):
         Dec0                        = self.parameters['Dec0'].value
         sigma                       = self.parameters['sigma'](energy).value
         
-        return numpy.maximum( numexpr.evaluate("1./(2 * pi * sigma**2) * exp(-0.5 * ((RA-RA0)**2 + (Dec - Dec0)**2)/sigma**2)"), 1e-30)
+        return numpy.maximum( np.power(180/np.pi,2)*1./(2 * np.pi * sigma**2) * np.exp(-0.5 * np.power(angsep(RA,Dec,ra0,dec0),2)/sigma**2), 1e-30)
 
 
 class MultiVariateGaussian(SpatialModel):
     
     def setup(self):
         self.functionName        = "Multivariate Gaussian"
-        self.formula             = r'$f(\vec{x}) = \frac{1}{2\pi \sqrt{\det{\Sigma}}} \, {\rm exp}\left( -\frac{1}{2} (\vec{x}-\vec{x}_0)^\intercal \cdot \Sigma^{-1}\cdot (\vec{x}-\vec{x}_0)\right) \\ \vec{x}_0 = ({\rm RA}_0,{\rm Dec}_0)\\ \Lambda = \left( \begin{array}{cc} \sigma & 0 \\ 0 & \sigma \sqrt{1-e^2} \end{array}\right) \\ U = \left( \begin{array}{cc} \cos \theta & -\sin \theta \\ \sin \theta & cos \theta \end{array}\right) \\\Sigma = U\Lambda U^\intercal$'
+        self.formula             = r'$f(\vec{x}) = \left(\frac{180^\circ}{\pi}\right)^2 \frac{1}{2\pi \sqrt{\det{\Sigma}}} \, {\rm exp}\left( -\frac{1}{2} (\vec{x}-\vec{x}_0)^\intercal \cdot \Sigma^{-1}\cdot (\vec{x}-\vec{x}_0)\right) \\ \vec{x}_0 = ({\rm RA}_0,{\rm Dec}_0)\\ \Lambda = \left( \begin{array}{cc} \sigma^2 & 0 \\ 0 & \sigma^2 (1-e^2) \end{array}\right) \\ U = \left( \begin{array}{cc} \cos \theta & -\sin \theta \\ \sin \theta & cos \theta \end{array}\right) \\
+        \Sigma = U\Lambda U^\intercal$'
         self.parameters          = collections.OrderedDict()
         self.parameters['RA0']     = Parameter('RA0',1.,0,360,0.1,fixed=False,nuisance=False,dataset=None)
         self.parameters['Dec0']     = Parameter('Dec0',1.,-90,90,0.1,fixed=False,nuisance=False,dataset=None)
@@ -58,8 +59,8 @@ class MultiVariateGaussian(SpatialModel):
         sigma_1=sigma
         sigma_2=sigma*math.sqrt(1-eccentricity**2)
         rot=np.array([[np.cos(angle),-np.sin(angle)],[np.sin(angle),np.cos(angle)]])
-        cov=np.dot(rot,np.dot(np.array([[sigma_1,0],[0,sigma_2]]),rot.T))
-        return numpy.maximum(scipy.stats.multivariate_normal.pdf(np.array([RA,Dec]).T, mean=np.array([RA0,Dec0]).T, cov=cov), 1e-30)
+        cov=np.dot(rot,np.dot(np.array([[sigma_1**2,0],[0,sigma_2**2]]),rot.T))
+        return numpy.maximum(np.power(180/np.pi,2)*scipy.stats.multivariate_normal.pdf(np.array([RA,Dec]).T, mean=np.array([RA0,Dec0]).T, cov=cov), 1e-30)
    
   
 
