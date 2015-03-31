1ef4b9e4da3e7e9f8c1f3aff45f90555c71a2b9b
tibaldo <ltibaldo@slac.stanford.edu>
2015-03-31 13:55:55 -0700

    added ring spatial model
    also fixed typos/bugs in ellipse

--- threeML/models/spatialModels/ellipse.py
+++ threeML/models/spatialModels/ellipse.py
@@ -5,8 +5,8 @@ import numpy as np
 class Ellipse(SpatialModel):
     
     def setup(self):
-        self.functionName        = "Disk"
-        self.formula             = r'$f({\rm RA, Dec}) = \left(\frac{180^\circ}{\pi}\right)^2 \frac{1}{\pi r^2} \, \left[ \frac{\cos\theta \,{\rm RA} + \sin\theta\, {\rm Dec} - {\rm RA_0}}{a^2} + \frac{-\sin\theta\, {\rm RA} + \cos \theta \,{\rm Dec}- {\rm Dec_0}}{a^2 (1-e^2)} < 1 \right] $'
+        self.functionName        = "Ellipse"
+        self.formula             = r'$f({\rm RA, Dec}) = \left(\frac{180^\circ}{\pi}\right)^2 \frac{1}{\pi a^2 \sqrt{1-e^2}} \, \left[ \left(\frac{\cos\theta \,{\rm RA} + \sin\theta\, {\rm Dec} - {\rm RA_0}}{a^2} + \frac{-\sin\theta\, {\rm RA} + \cos \theta \,{\rm Dec}- {\rm Dec_0}}{a^2 (1-e^2)}\right) < 1 \right]$'
         self.parameters          = collections.OrderedDict()
         self.parameters['RA0']     = Parameter('RA0',1.,0,360,0.1,fixed=False,nuisance=False,dataset=None)
         self.parameters['Dec0']     = Parameter('Dec0',1.,-90,90,0.1,fixed=False,nuisance=False,dataset=None)
@@ -23,7 +23,7 @@ class Ellipse(SpatialModel):
         Dec0                        = self.parameters['Dec0'].value
         maj_saxis                   = self.parameters['maj_saxis'](energy).value
         eccentricity                = self.parameters['eccentricity'](energy).value
-        angle                       = np.deg2rad(self.parameters['angle'](energy).value)
+        angle                       = self.parameters['angle'](energy).value
         
         s=np.sin(np.deg2rad(angle))
         c=np.cos(np.deg2rad(angle))
