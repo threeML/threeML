e69eec783b2b2732672a79d906851e2495dc575f
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-05-29 17:26:56 -0700

    Fixed a bug which was preventing the printing of the values for the parameters resulting in a NaN for the likelihood

--- threeML/classicMLE/jointLikelihood.py
+++ threeML/classicMLE/jointLikelihood.py
@@ -13,6 +13,58 @@ from astropy.table import Table
 
 from IPython.display import display
 
+import numpy as np
+
+def cartesian(arrays, out=None):
+    """
+    Generate a cartesian product of input arrays.
+
+    Parameters
+    ----------
+    arrays : list of array-like
+        1-D arrays to form the cartesian product of.
+    out : ndarray
+        Array to place the cartesian product in.
+
+    Returns
+    -------
+    out : ndarray
+        2-D array of shape (M, len(arrays)) containing cartesian products
+        formed of input arrays.
+
+    Examples
+    --------
+    >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
+    array([[1, 4, 6],
+           [1, 4, 7],
+           [1, 5, 6],
+           [1, 5, 7],
+           [2, 4, 6],
+           [2, 4, 7],
+           [2, 5, 6],
+           [2, 5, 7],
+           [3, 4, 6],
+           [3, 4, 7],
+           [3, 5, 6],
+           [3, 5, 7]])
+
+    """
+
+    arrays = [np.asarray(x) for x in arrays]
+    dtype = arrays[0].dtype
+
+    n = np.prod([x.size for x in arrays])
+    if out is None:
+        out = np.zeros([n, len(arrays)], dtype=dtype)
+
+    m = n / arrays[0].size
+    out[:,0] = np.repeat(arrays[0], m)
+    if arrays[1:]:
+        cartesian(arrays[1:], out=out[0:m,1:])
+        for j in xrange(1, arrays[0].size):
+            out[j*m:(j+1)*m,1:] = out[0:m,1:]
+    return out
+
 class JointLikelihood(object):
   
   def __init__(self, likelihoodModel, dataList, **kwargs):
@@ -43,6 +95,38 @@ class JointLikelihood(object):
     
   pass
   
+  def preFit(self):
+    
+    #This is a simple scan through the normalization parameters to guess
+    #a good starting point for them (if we start too far from the data,
+    #minuit and the other minimizers will have trouble)
+    
+    freeParams               = self.likelihoodModel.getFreeParameters()
+    
+    normParameters           = filter( lambda x : x.isNormalization() , freeParams.values() )
+    
+    self.freeParameters      = {}
+    
+    #Prepare the grid
+    
+    grids                    = []
+    
+    for norm in normParameters:
+      
+      self.freeParameters[norm.name()] = norm
+      
+      grids.append(numpy.logspace( numpy.log10( norm.minValue ),
+                                   numpy.log10( norm.maxValue ), 
+                                   50 )
+      
+    #Compute the global likelihood at each point in the grid
+    globalGrid               = cartesian(grids)
+    
+    logLikes                 = map(minuxLogLikeProfile, globalGrid)
+    
+    idx                      = numpy.argmin(logLikes)
+    print("Minimum is %s with %s" %(logLikes[idx],globalGrid[idx]))
+    
   def minusLogLikeProfile(self, *trialValues):
       
       #Keep track of the number of calls
