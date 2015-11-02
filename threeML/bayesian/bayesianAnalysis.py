08dc9dbd4ac900cd373fc24dba8f0ec751a2c5ed
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-11-02 08:58:56 -0800

    Implemented Parallel Tempering

--- threeML/bayesian/bayesianAnalysis.py
+++ threeML/bayesian/bayesianAnalysis.py
@@ -6,6 +6,8 @@ from threeML.io.ProgressBar import ProgressBar
 
 from threeML.io.triangle import corner
 
+from threeML.exceptions.CustomExceptions import ModelAssertionViolation
+
 import emcee
 import emcee.utils
 
@@ -52,10 +54,58 @@ class bayesianAnalysis( object ):
           
           dataset.setModel(self.likelihoodModel)
     
+    def _logp( self, trialValues ):
+        
+        #Compute the sum of the log-priors
+        
+        logp = 0
+        
+        for i,( srcName, paramName ) in enumerate( self.freeParameters.keys() ):
+            
+            thisParam = self.likelihoodModel.parameters[ srcName ][ paramName ]
+            
+            logp += thisParam.prior( trialValues[i] )
+            
+        return logp
+        
+    def _logLike( self, trialValues ):
+        
+        #Compute the log-likelihood
+                
+        for i,( srcName, paramName ) in enumerate( self.freeParameters.keys() ):
+            
+            thisParam = self.likelihoodModel.parameters[ srcName ][ paramName ]
+            
+            thisParam.setValue( trialValues[i] )
+        
+        try:    
+            
+            logLike = numpy.sum( map( lambda dataset: dataset.getLogLike(), self.dataList.values() ) )
+        
+        except ModelAssertionViolation:
+            
+            return -numpy.inf
+        
+        except:
+            
+            raise
+        
+        if not numpy.isfinite( logLike ):
+            
+            return -numpy.inf
+        
+        else:
+            
+            return logLike
+    
     def posterior( self, trialValues ):
+        
+        #Here we don't use the self._logp nor the self._logLike to
+        #avoid looping twice over the parameters (for speed)
         
         #Assign this trial values to the parameters and
         #store the corresponding values for the priors
+        
         lps = []
         
         for i,( srcName, paramName ) in enumerate( self.freeParameters.keys() ):
@@ -82,19 +132,14 @@ class bayesianAnalysis( object ):
         
         return logLike + logPrior
     
-    def sample( self, nwalkers, burn_in, nsamples ):
+    def _getStartingPoint( self, nwalkers, variance = 0.1 ):
         
-        self.freeParameters = self.likelihoodModel.getFreeParameters()
-                
-        ndim = len( self.freeParameters.keys() )
-                
         #Generate the starting points for the walkers by getting random
         #values for the parameters close to the current value
         
         #Fractional variance for randomization
         #(0.1 means var = 0.1 * value )
-        variance = 0.1
-        
+                
         p0 = [ ]
         
         for i in range( nwalkers ):
@@ -111,6 +156,57 @@ class bayesianAnalysis( object ):
             
             p0.append( thisP0 )
         
+        return p0     
+        
+    
+    def samplePT( self, ntemps, nwalkers, burn_in, nsamples ):
+        '''
+        Sample with parallel tempering
+        '''
+        
+        self.freeParameters = self.likelihoodModel.getFreeParameters()
+                
+        ndim = len( self.freeParameters.keys() )
+        
+        sampler = emcee.PTSampler( ntemps, nwalkers, ndim, self._logLike, self._logp )
+        
+        #Get one starting point for each temperature
+        
+        p0 = numpy.empty( ( ntemps, nwalkers, ndim ) )
+        
+        for i in range( ntemps ):
+            
+            p0[i,:,:] = self._getStartingPoint( nwalkers )
+        
+        print("Running burn-in of %s samples...\n" % burn_in )
+        
+        p, lnprob, lnlike = sampleWithProgress( p0, sampler, burn_in )
+        
+        #Reset sampler
+        
+        sampler.reset()
+        
+        print("\nSampling...\n")
+        
+        p, lnprob, lnlike = sampleWithProgress( p, sampler, nsamples, 
+                                                lnprob0 = lnprob, lnlike0 = lnlike )
+        
+        self.sampler = sampler
+        self.samples = sampler.flatchain.reshape(-1, sampler.flatchain.shape[-1])
+        
+        return self.getSamples()
+        
+    
+    def sample( self, nwalkers, burn_in, nsamples ):
+        
+        self.freeParameters = self.likelihoodModel.getFreeParameters()
+                
+        ndim = len( self.freeParameters.keys() )
+        
+        #Get starting point
+        
+        p0 = self._getStartingPoint( nwalkers )
+        
         if threeML_config['parallel']['use-parallel']:
         
             c = ParallelClient()
@@ -125,9 +221,8 @@ class bayesianAnalysis( object ):
             sampler = emcee.EnsembleSampler( nwalkers, ndim, 
                                              self.posterior )
         
-        print("Running burn-in of %s samples..." % burn_in )
-
-            
+        print("Running burn-in of %s samples...\n" % burn_in )
+        
         pos, prob, state = sampleWithProgress( p0, sampler, burn_in )
         
         #Reset sampler
@@ -136,15 +231,9 @@ class bayesianAnalysis( object ):
         
         #Run the true sampling
         
-        print("Sampling...")
-        
-        beg = time.time()
-        
+        print("\nSampling...\n")
+                
         _ = sampleWithProgress( pos, sampler, nsamples, rstate0=state ) 
-        
-        end = time.time()
-        
-        print("%s" %(end - beg))
         
         #sampler.run_mcmc( pos, nsamples, rstate0=state )
         
@@ -153,16 +242,23 @@ class bayesianAnalysis( object ):
         print( "Mean acceptance fraction: %s" % acc )
         
         self.sampler = sampler
+        self.samples = sampler.flatchain
         
         return self.getSamples()
     
     def getSamples( self ):
         
-        return self.sampler.flatchain
+        if hasattr( self, "samples" ):
+            
+            return self.samples
+        
+        else:
+            
+            raise RuntimeError("You have to run the sampler first, using the sample() method")
     
     def cornerPlot( self, **kwargs ):
         
-        if hasattr( self, "sampler" ):
+        if hasattr( self, "samples" ):
             
             labels = []
             priors = []
@@ -175,7 +271,7 @@ class bayesianAnalysis( object ):
                 
                 priors.append( self.likelihoodModel.parameters[ srcName ][ paramName ].prior )
             
-            fig = corner( self.sampler.flatchain, labels=labels, 
+            fig = corner( self.samples, labels=labels, 
                           quantiles=[0.16, 0.50, 0.84],
                           priors = priors, **kwargs )
             
