94cf71190525ff097a52e8f35dc246edef27f3b7
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-12-10 16:19:24 -0800

    Fixed a conflict with astropy when importing in a regular python (instead of ipython)

--- threeML/__init__.py
+++ threeML/__init__.py
@@ -28,19 +28,6 @@ def __import__(name, globals=None, locals=None, fromlist=None):
   pass
 pass
 
-#Find the directory containing 3ML
-threeML_dir                   = os.path.abspath(os.path.dirname(__file__))
-
-#Import all modules here
-sys.path.insert(0,threeML_dir)
-mods                          = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.join(threeML_dir,"*.py"))]
-#Filter out __init__
-modsToImport                  = filter(lambda x:x.find("__init__")<0,mods)
-
-#Import everything in current directory
-for mod in modsToImport:
-  exec("from %s import *" %(mod))
-
 #Import in the current namespace everything under the 
 #models directory
 from .models.PointSource import PointSource
@@ -60,6 +47,20 @@ from .classicMLE.jointLikelihood import JointLikelihood
 
 #Import the DataList class
 from dataList import DataList
+
+
+#Find the directory containing 3ML
+threeML_dir                   = os.path.abspath(os.path.dirname(__file__))
+
+#Import all modules here
+sys.path.insert(0,threeML_dir)
+mods                          = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.join(threeML_dir,"*.py"))]
+#Filter out __init__
+modsToImport                  = filter(lambda x:x.find("__init__")<0,mods)
+
+#Import everything in current directory
+for mod in modsToImport:
+  exec("from %s import *" %(mod))
 
 #Now look for plugins
 plugins_dir                   = os.path.join(os.path.dirname(__file__),"plugins")
