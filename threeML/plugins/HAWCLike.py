8932b22be81574f687595769e29e8203b9c97e28
giacomov <giacomo.vianello@gmail.com>
2016-06-14 20:37:25 -0700

    Added back support for point sources

--- threeML/plugins/HAWCLike.py
+++ threeML/plugins/HAWCLike.py
@@ -247,7 +247,7 @@ class HAWCLike(PluginPrototype):
 
         # Get the energies needed by LiFF (the same for all sources)
 
-        self._energies = np.array(self.theLikeHAWC.GetEnergies(False)) * 1e6
+        self._energies = np.array(self.theLikeHAWC.GetEnergies(False)) * 1000.0
 
     def _CommonNormCallback(self, value):
 
