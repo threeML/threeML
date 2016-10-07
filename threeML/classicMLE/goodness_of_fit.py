b8730bae82ec517269ad8e3aa3581b8266b0ce81
giacomov <giacomo.vianello@gmail.com>
2016-10-07 10:03:10 -0700

    Now computing all goodness of fit

--- threeML/classicMLE/goodness_of_fit.py
+++ threeML/classicMLE/goodness_of_fit.py
@@ -17,6 +17,31 @@ class GoodnessOfFit(object):
 
         self._reference_like = like_data_frame['-log(likelihood)']
 
+    def get_simulated_data(self, id):
+
+        # Generate a new data set for each plugin contained in the data list
+
+        new_datas = []
+
+        for dataset in self._jl_instance.data_list.values():
+
+            new_data = dataset.get_simulated_dataset("%s_sim" % dataset.name)
+
+            new_datas.append(new_data)
+
+        new_data_list = DataList(*new_datas)
+
+        return new_data_list
+
+    def get_model(self, id):
+
+        # Make a copy of the best fit model, so that we don't touch the original model during the fit, and we
+        # also always restart from the best fit (instead of the last iteration)
+
+        new_model = clone_model(self._jl_instance.likelihood_model)
+
+        return new_model
+
     def by_mc(self, n_iterations=1000, continue_on_failure=False):
         """
         Compute goodness of fit by generating Monte Carlo datasets and fitting the current model on them. The fraction
@@ -28,35 +53,8 @@ class GoodnessOfFit(object):
         :return: tuple (goodness of fit, frame with all results, frame with all likelihood values)
         """
 
-        # Define the get_data and the get_model functions for the JointLikelihoodSet instance
-
-        def get_data(id):
-
-            # Generate a new data set for each plugin contained in the data list
-
-            new_datas = []
-
-            for dataset in self._jl_instance.data_list.values():
-
-                new_data = dataset.get_simulated_dataset("%s_sim" % dataset.name)
-
-                new_datas.append(new_data)
-
-            new_data_list = DataList(*new_datas)
-
-            return new_data_list
-
-        def get_model(id):
-
-            # Make a copy of the best fit model, so that we don't touch the original model during the fit, and we
-            # also always restart from the best fit (instead of the last iteration)
-
-            new_model = clone_model(self._jl_instance.likelihood_model)
-
-            return new_model
-
         # Create the joint likelihood set
-        jl_set = JointLikelihoodSet(get_data, get_model, n_iterations, iteration_name='simulation')
+        jl_set = JointLikelihoodSet(self.get_simulated_data, self.get_model, n_iterations, iteration_name='simulation')
 
         # Use the same minimizer as in the joint likelihood object
 
