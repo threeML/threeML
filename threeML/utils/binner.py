e19483f8e465a44eafc53ee8ca1841ef48abf4a2
J. Michael Burgess <jmichaelburgess@gmail.com>
2016-10-18 21:12:58 +0200

    transferred giacomo's bayes blocks function

--- threeML/utils/binner.py
+++ threeML/utils/binner.py
@@ -1,4 +1,6 @@
 import numpy as np
+import numexpr
+
 from threeML.utils.stats_tools import Significance
 from threeML.io.progress_bar import progress_bar
 
@@ -353,17 +355,190 @@ class TemporalBinner(object):
         self._starts = tmp
         self._stops = tmp + dt
 
-    def bin_by_bayesian_blocks(self, p0):
+    def bin_by_bayesian_blocks(self, p0, bkg_integral_distribution=None, my_likelihood=None):
+        """Divide a series of events characterized by their arrival time in blocks
+        of perceptibly constant count rate. If the background integral distribution
+        is given, divide the series in blocks where the difference with respect to
+        the background is perceptibly constant.
+        Args:
+          self._arrival_times (iterable): An iterable (list, numpy.array...) containing the arrival
+                         time of the events.
+                         NOTE: the input array MUST be time-ordered, and without
+                         duplicated entries. To ensure this, you may execute the
+                         following code:
+                         tt_array = numpy.asarray(self._arrival_times)
+                         tt_array = numpy.unique(tt_array)
+                         tt_array.sort()
+                         before running the algorithm.
+          p0 (float): The probability of finding a variations (i.e., creating a new
+                      block) when there is none. In other words, the probability of
+                      a Type I error, i.e., rejecting the null-hypothesis when is
+                      true. All found variations will have a post-trial significance
+                      larger than p0.
+          bkg_integral_distribution (function, optional): the integral distribution for the
+                      background counts. It must be a function of the form f(x),
+                      which must return the integral number of counts expected from
+                      the background component between time 0 and x.
+        Returns:
+          numpy.array: the edges of the blocks found
         """
 
-        Bin using the Bayesian block algorithm of Scargle et al. 2013.
+        # Verify that the input array is one-dimensional
 
 
-        :param p0:
-        :return:
-        """
 
-        raise NotImplementedError('Bayesian blocks is not implemented yet')
+        t_start = self._arrival_times[0]
+        t_stop = self._arrival_times[-1]
+
+        if (bkg_integral_distribution is not None):
+            # Transforming the inhomogeneous Poisson process into an homogeneous one with rate 1,
+            # by changing the time axis according to the background rate
+
+            t = np.array(bkg_integral_distribution(self._arrival_times))
+
+            # Now compute the start and stop time in the new system
+            tstart = bkg_integral_distribution(t_stop)
+            tstop = bkg_integral_distribution(t_stop)
+        else:
+            t = self._arrival_times
+            tstart = t_start
+            tstop = t_stop
+        pass
+
+        # Create initial cell edges (Voronoi tessellation)
+        edges = np.concatenate([[tstart],
+                                0.5 * (t[1:] + t[:-1]),
+                                [tstop]])
+
+        # Create the edges also in the original time system
+        edges_ = np.concatenate([[t_start],
+                                 0.5 * (self._arrival_times[1:] + self._arrival_times[:-1]),
+                                 [t_stop]])
+
+        # Create a lookup table to be able to transform back from the transformed system
+        # to the original one
+        lookup_table = {key: value for (key, value) in zip(edges, edges_)}
+
+        # The last block length is 0 by definition
+        block_length = tstop - edges
+
+        if np.sum((block_length <= 0)) > 1:
+
+            raise RuntimeError("Events appears to be out of order! Check for order, or duplicated events.")
+
+        N = t.shape[0]
+
+        # arrays to store the best configuration
+        best = np.zeros(N, dtype=float)
+        last = np.zeros(N, dtype=int)
+        best_new = np.zeros(N, dtype=float)
+        last_new = np.zeros(N, dtype=int)
+
+        # Pre-computed priors (for speed)
+
+        if my_likelihood:
+
+            priors = my_likelihood.getPriors(N, p0)
+
+        else:
+
+            # eq. 21 from Scargle 2012
+            priors = 4 - np.log(73.53 * p0 * np.power(np.arange(1, N + 1), -0.478))
+        pass
+
+        x = np.ones(N)
+
+        # Speed tricks: resolve once for all the functions which will be used
+        # in the loop
+        cumsum = np.cumsum
+        log = np.log
+        argmax = np.argmax
+        numexpr_evaluate = numexpr.evaluate
+        arange = np.arange
+
+        # Decide the step for reporting progress
+        # incr = max(int(float(N) / 100.0 * 10), 1)
+
+
+        # This is where the computation happens. Following Scargle et al. 2012.
+        # This loop has been optimized for speed:
+        # * the expression for the fitness function has been rewritten to
+        #  avoid multiple log computations, and to avoid power computations
+        # * the use of scipy.weave and numexpr has been evaluated. The latter
+        #  gives a big gain (~40%) if used for the fitness function. No other
+        #  gain is obtained by using it anywhere else
+
+        times = []
+        TSs = []
+
+        # Set numexpr precision to low (more than enough for us), which is
+        # faster than high
+        old_accuracy = numexpr.set_vml_accuracy_mode('low')
+        numexpr.set_num_threads(1)
+        numexpr.set_vml_num_threads(1)
+
+        for R in range(N):
+            br = block_length[R + 1]
+            T_k = block_length[:R + 1] - br
+
+            # N_k: number of elements in each block
+            # This expression has been simplified for the case of
+            # unbinned events (i.e., one element in each block)
+            # It was:
+            # N_k = cumsum(x[:R + 1][::-1])[::-1]
+            # Now it is:
+            N_k = arange(R + 1, 0, -1)
+
+            # Evaluate fitness function
+            # This is the slowest part, which I'm speeding up by using
+            # numexpr. It provides a ~40% gain in execution speed.
+
+            fit_vec = numexpr_evaluate('''N_k * log(N_k/ T_k) ''',
+                                       optimization='aggressive')
+
+            p = priors[R]
+
+            A_R = fit_vec - p
+
+            A_R[1:] += best[:R]
+
+            i_max = argmax(A_R)
+
+            last[R] = i_max
+            best[R] = A_R[i_max]
+
+        numexpr.set_vml_accuracy_mode(old_accuracy)
+
+        # Now find blocks
+        change_points = np.zeros(N, dtype=int)
+        i_cp = N
+        ind = N
+        while True:
+            i_cp -= 1
+            change_points[i_cp] = ind
+
+            if ind == 0:
+                break
+
+            ind = last[ind - 1]
+
+        change_points = change_points[i_cp:]
+
+        edg = edges[change_points]
+
+        # Transform the found edges back into the original time system
+        if bkg_integral_distribution is not None:
+            final_edges = map(lambda x: lookup_table[x], edg)
+        else:
+            finalEdges = edg
+
+        self._starts = np.asarray(finalEdges)[:-1]
+        self._starts = np.asarray(finalEdges)[1:]
+
+        # return np.asarray(finalEdges)
+
+
+
 
     def bin_by_custom(self, start, stop):
         """
