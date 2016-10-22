8b4a7014e13a9a6fcb87705ec9c4013d3e9e28aa
J. Michael Burgess <jmichaelburgess@gmail.com>
2016-10-22 20:24:56 +0200

    added bayesian test for GBM data

--- threeML/test/test_gbm.py
+++ threeML/test/test_gbm.py
@@ -12,6 +12,7 @@ from astromodels.model import Model
 from astromodels.functions.functions import Powerlaw, Exponential_cutoff
 from astromodels.sources.point_source import PointSource
 from astromodels.sources.extended_source import ExtendedSource
+from astromodels.functions.functions import Log_uniform_prior, Uniform_prior
 
 
 from threeML.io.file_utils import within_directory
@@ -40,6 +41,35 @@ class AnalysisBuilder(object):
 
         return self._shapes.keys()
 
+    def set_priors(self):
+
+        key = 'normal'
+
+        self._shapes[key].K.prior = Log_uniform_prior(lower_bound=1e-10, upper_bound=1e2)
+        self._shapes[key].index.set_uninformative_prior(Uniform_prior)
+
+        key = 'add'
+
+        self._shapes[key].K_1.prior = Log_uniform_prior(lower_bound=1e-10, upper_bound=1e2)
+        self._shapes[key].index_1.set_uninformative_prior(Uniform_prior)
+        self._shapes[key].K_2.prior = Log_uniform_prior(lower_bound=1e-10, upper_bound=1e2)
+        self._shapes[key].index_2.set_uninformative_prior(Uniform_prior)
+
+        key = 'mult'
+
+        self._shapes[key].K_1.prior = Log_uniform_prior(lower_bound=1e-10, upper_bound=1e2)
+        self._shapes[key].index_1.set_uninformative_prior(Uniform_prior)
+        self._shapes[key].K_2.prior = Log_uniform_prior(lower_bound=1e-10, upper_bound=1e2)
+        self._shapes[key].xc_2.prior = Log_uniform_prior(lower_bound=1e0, upper_bound=1e2)
+
+        key = 'crazy'
+
+        self._shapes[key].K_1.prior = Log_uniform_prior(lower_bound=1e-10, upper_bound=1e2)
+        self._shapes[key].xc_1.prior = Log_uniform_prior(lower_bound=1e0, upper_bound=1e2)
+        self._shapes[key].K_2.prior = Log_uniform_prior(lower_bound=1e-10, upper_bound=1e2)
+        self._shapes[key].index_2.set_uninformative_prior(Uniform_prior)
+        self._shapes[key].K_3.prior = Log_uniform_prior(lower_bound=1e-10, upper_bound=1e2)
+        self._shapes[key].index_3.set_uninformative_prior(Uniform_prior)
 
 
 
@@ -64,14 +94,15 @@ class AnalysisBuilder(object):
 
         data_list = DataList(self._plugin)
 
-        jls = {}
+        bayes = {}
 
         for key in self._shapes.keys():
             ps = PointSource('test', 0, 0, spectral_shape=self._shapes[key])
             model = Model(ps)
-            jls[key] = JointLikelihood(model, data_list)
 
-        return jls
+            bayes[key] = BayesianAnalysis(model, data_list)
+
+        return bayes
 
 
 
@@ -146,3 +177,61 @@ def test_gbm_tte_joint_likelihood_fitting():
 
             assert jl.minimizer is not None
             assert jl.ncalls != 1
+
+
+def test_gbm_tte_bayesian_fitting():
+    with within_directory(__example_dir):
+        data_dir = os.path.join('gbm', 'bn080916009')
+
+        src_selection = "0.-10."
+
+        nai3 = FermiGBMTTELike('NAI3',
+                               os.path.join(data_dir, "glg_tte_n3_bn080916009_v01.fit.gz"),
+                               "-10-0, 100-150",
+                               src_selection,
+                               rsp_file=os.path.join(data_dir, "glg_cspec_n3_bn080916009_v07.rsp"), poly_order=-1)
+
+        ab = AnalysisBuilder(nai3)
+        ab.set_priors()
+        bayes = ab.build_point_source_bayes()
+
+        for key in ['normal']:
+
+            bb = bayes[key]
+
+            assert bb.analysis_type == 'bayesian'
+            assert bb._samples is None
+            assert bb._raw_samples is None
+            assert bb._sampler is None
+            assert bb._log_like_values is None
+
+            n_walk = 10
+            n_samp = 100
+
+            n_samples = n_walk * n_samp
+
+            samples = bb.sample(n_walkers=n_walk, burn_in=10, n_samples=n_samp)
+
+            for key_2 in samples.keys():
+                assert samples[key_2].shape[0] == n_samples
+
+            assert len(bb.log_like_values) == n_samples
+            # assert len(bb.log_probability_values) == n_samples
+
+            assert bb.raw_samples.shape == (n_samples, 2)
+
+
+
+
+            # assert bb.current_minimum is None
+            # assert jl.minimizer_in_use == ('MINUIT', None)
+            # assert jl.minimizer is None
+            # assert jl.ncalls == 0
+            # assert jl.verbose == False
+
+            # res, _ = jl.fit()
+
+            # assert jl.current_minimum is not None
+
+            # assert jl.minimizer is not None
+            # assert jl.ncalls != 1
