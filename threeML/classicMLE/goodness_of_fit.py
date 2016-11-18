10d4ef757978925931734b1fd8004329d5dcc803
J. Michael Burgess <jmichaelburgess@gmail.com>
2016-11-18 13:06:03 +0100

    added callback to goodness of fit minimizer setting

--- threeML/classicMLE/goodness_of_fit.py
+++ threeML/classicMLE/goodness_of_fit.py
@@ -58,8 +58,8 @@ class GoodnessOfFit(object):
 
         # Use the same minimizer as in the joint likelihood object
 
-        minimizer_name, algorithm = self._jl_instance.minimizer_in_use
-        jl_set.set_minimizer(minimizer_name, algorithm)
+        minimizer_name, algorithm, callback = self._jl_instance.minimizer_in_use
+        jl_set.set_minimizer(minimizer_name, algorithm, callback)
 
         # Run the set
         data_frame, like_data_frame = jl_set.go(continue_on_failure=continue_on_failure)
