3a653bc9d0a328e6389bb52281df153c17b7e0ff
giacomov <giacomo.vianello@gmail.com>
2016-12-14 13:26:45 -0800

    useless changes

--- threeML/config/config_checker.py
+++ threeML/config/config_checker.py
@@ -1,61 +1,9 @@
-__author__ = 'drJfunk'
-
 # from threeML.config.config import threeML_config
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 
 from threeML.exceptions.custom_exceptions import custom_warnings
 from threeML.exceptions import custom_exceptions
-
-# Note! These must bne updated as new configuration variables
-# are introduced!
-
-
-_color_keys = [['ogip', 'counts color'],
-               ['ogip', 'background color'],
-               ['gbm', 'lightcurve color'],
-               ['gbm', 'selection color'],
-               ['gbm', 'background color'],
-               ['gbm', 'background selection color'],
-               ['mle', 'contour background'],
-               ['mle', 'contour level 1'],
-               ['mle', 'contour level 2'],
-               ['mle', 'contour level 3'],
-               ['mle', 'profile color'],
-               ['mle', 'profile level 1'],
-               ['mle', 'profile level 2'],
-               ['mle', 'profile level 3']
-               ]
-
-_cmap_keys = [['ogip', 'data plot cmap'],
-              ['ogip', 'model plot cmap'],
-              ['mle', 'contour cmap'],
-              ['model plot', 'fit cmap'],
-              ['model plot', 'bayes cmap'],
-              ['model plot', 'contour cmap']
-              ]
-
-# _parallel_keys = [['parallel', 'IPython profile name'],
-#                   ['parallel', 'use-parallel']]
-
-
-_bool_keys = [['parallel', 'use-parallel']]
-
-_string_keys = [['parallel', 'IPython profile name']]
-
-# This stores all the keys that MUST be in the configuration file
-
-_required_keys = {}
-
-# build a dictionary of the keys
-for element in [_bool_keys, _string_keys, _color_keys, _cmap_keys]:
-
-    for key_pair in element:
-
-        _required_keys.setdefault(key_pair[0], []).append(key_pair[1])
-
-
-
 
 
 def is_matplotlib_cmap(cmap):
@@ -101,6 +49,9 @@ def check_configuration(threeML_config, path):
 
     :return:
     """
+
+    # First get the configuration file from the distribution root (which is *assumed* to be correct)
+
 
     configuration_is_ok = True
 
