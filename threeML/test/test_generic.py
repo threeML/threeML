96ce08ffeec6007b1832ba7679032c9071e7dd70
J. Michael Burgess <jmichaelburgess@gmail.com>
2016-12-18 10:48:35 +0100

    added test for step gen

--- threeML/test/test_generic.py
+++ threeML/test/test_generic.py
@@ -1,3 +1,22 @@
 import pytest
 
-__author__ = 'drjfunk'
+from threeML import *
+
+
+def test_step_generator_setup():
+    ra, dec = 0, 0
+    name = "test"
+
+    powerlaw = Powerlaw()
+
+    line = Line()
+
+    ps = PointSource(name, ra, dec, spectral_shape=powerlaw)
+
+    model = Model(ps)
+
+    # test with
+
+    step = step_generator([1, 2, 3, 4, 5], powerlaw.K)
+
+    step = step_generator([[1, 2], [3, 4]], powerlaw.K)
