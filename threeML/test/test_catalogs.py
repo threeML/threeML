bf4b5cad5828b1ad5dcc6fc872487b96565f0805
giacomov <giacomo.vianello@gmail.com>
2016-12-21 11:00:33 -0800

    Testing catalogs

---
+++ threeML/test/test_catalogs.py
@@ -0,0 +1,29 @@
+import pytest
+
+from threeML import *
+
+
+def test_GBM_catalog():
+
+    gbm_catalog = FermiGBMBurstCatalog()
+
+    _ = gbm_catalog.cone_search(0.0, 0.0, 5.0)
+
+    assert gbm_catalog.ra_center == 0.0
+    assert gbm_catalog.dec_center == 0.0
+
+    gbm_catalog.search_around_source('Crab', 5.0)
+
+
+def test_LAT_catalog():
+
+    lat_catalog = FermiLATSourceCatalog()
+
+    ra, dec, table1 = lat_catalog.search_around_source('Crab', 1.0)
+
+    table2 = lat_catalog.cone_search(ra, dec, 1.0)
+
+    assert len(table1) == len(table2)
+
+    assert lat_catalog.ra_center == ra
+    assert lat_catalog.dec_center == dec
