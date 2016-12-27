26e673a35d030286c8abf31b75d2024b9929d34f
giacomov <giacomo.vianello@gmail.com>
2016-12-26 22:52:08 -0800

    Added ipython

--- install_3ML.py
+++ install_3ML.py
@@ -228,7 +228,7 @@ if __name__ == "__main__":
 
         f.write("#/bin/bash\n")
         f.write("source %s/bin/activate\n" % env_path)
-        f.write("pip install numpy scipy matplotlib iminuit astropy --upgrade\n")
+        f.write("pip install numpy scipy matplotlib iminuit astropy ipython ipyparallel --upgrade\n")
         f.write("pip install git+https://github.com/giacomov/3ML.git --upgrade\n")
         f.write("pip install git+https://github.com/giacomov/astromodels.git --upgrade\n")
         f.write("pip install git+https://github.com/giacomov/cthreeML.git || "
