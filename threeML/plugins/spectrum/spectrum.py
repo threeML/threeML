5740fa9c9bf576e7ad2df128f30845121e6bd63c
grburgess <jmichaelburgess@gmail.com>
2017-01-14 09:15:52 +0100

    starting spectrum class

---
+++ threeML/plugins/spectrum/spectrum.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class Spectrum(object):
+
+    def __init__(self):
+
+        pass
+
+    def from_pha_file(self, pha_file_name, spectrum_number, file_type='observed'):
+
+        pass
+
+    def from_pha_instance(self, pha_instance, spectrum_number, file_type):
+
+        pass
+
+    def from_ROOT(self):
+
+        pass
+
+
+
+
