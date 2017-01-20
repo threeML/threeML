707cf1096d9e53503414b618aa7be440c3740dba
grburgess <jmichaelburgess@gmail.com>
2017-01-20 15:59:53 +0100

    adding from_event_list

--- threeML/plugins/OGIP/pha.py
+++ threeML/plugins/OGIP/pha.py
@@ -1233,6 +1233,42 @@ class POISSON_PHAII(PHAII):
 
         return spectrum_extension
 
+    @classmethod
+    def from_event_list(cls, event_list, use_poly=False):
+
+        pha_information = event_list.get_pha_information(use_poly)
+
+        if use_poly:
+
+            return BAK_PHAII(instrument_name=pha_information['instrument'],
+                             telescope_name=pha_information['telescope'],
+                             tstart=pha_information['tstart'],
+                             telapse=pha_information['telapse'],
+                             channel=pha_information['channel'],
+                             rate=pha_information['rate'],
+                             stat_err=pha_information['rate error'],
+                             quality=pha_information['quality'],
+                             grouping=pha_information['grouping'],
+                             exposure=pha_information['exposure'],
+                             backscale=None,
+                             respfile=pha_information['response_file'],
+                             ancrfile=None)
+
+        else:
+
+            return POISSON_PHAII(instrument_name=pha_information['instrument'],
+                                 telescope_name=pha_information['telescope'],
+                                 tstart=pha_information['tstart'],
+                                 telapse=pha_information['telapse'],
+                                 channel=pha_information['channel'],
+                                 rate=pha_information['rate'],
+                                 quality=pha_information['quality'],
+                                 grouping=pha_information['grouping'],
+                                 exposure=pha_information['exposure'],
+                                 backscale=None,
+                                 respfile=pha_information['response_file'],
+                                 ancrfile=None)
+
 class BAK_PHAII(PHAII):
 
     def __init__(self, instrument_name, telescope_name, tstart, telapse, channel, rate, quality, grouping, exposure,
