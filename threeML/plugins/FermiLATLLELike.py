737c882b54e2e6a95ea34a754351aad9bdade170
grburgess <jmichaelburgess@gmail.com>
2017-01-26 12:42:25 +0100

    added proper import

--- threeML/plugins/FermiLATLLELike.py
+++ threeML/plugins/FermiLATLLELike.py
@@ -2,14 +2,13 @@ __author__ = 'grburgess'
 
 import astropy.io.fits as fits
 import numpy as np
-
+import collections
 import pandas as pd
 import warnings
 
 
 from threeML.plugins.EventListLike import EventListLike
 from threeML.plugins.OGIP.eventlist import EventListWithLiveTime
-from threeML.io.rich_display import display
 from threeML.utils.fermi_relative_mission_time import compute_fermi_relative_mission_times
 
 from threeML.io.plugin_plots import binned_light_curve_plot
