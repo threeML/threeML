733438f8d4e348d98e68a12f4112651b69f5ed8b
grburgess <jmichaelburgess@gmail.com>
2017-02-02 12:55:52 +0100

    hot fix adding in TELESCOPE keyword

--- threeML/plugins/OGIP/pha.py
+++ threeML/plugins/OGIP/pha.py
@@ -760,21 +760,30 @@ class PHAWrite(object):
                                                   self._respfile[key],
                                                   self._ancrfile[key])
 
-
+            # write the file
 
             fits_file.writeto(self._outfile_name[key], overwrite=overwrite)
 
 
-            if self._out_rsp:
+        if self._out_rsp:
+
+            # add the various responses needed
+
+            extensions = [EBOUNDS(self._out_rsp[0].ebounds)]
+
+            extensions.extend([SPECRESP_MATRIX(this_rsp.monte_carlo_energies, this_rsp.ebounds, this_rsp.matrix) for this_rsp in self._out_rsp])
+
+            for ext in extensions[1:]:
 
 
-                extensions = [EBOUNDS(self._out_rsp[0].ebounds)]
+                # Set telescope and instrument name
+                ext.hdu.header.set("TELESCOP", self._mission['pha'])
+                ext.hdu.header.set("INSTRUME", self._instrument['pha'])
 
-                extensions.extend([SPECRESP_MATRIX(this_rsp.monte_carlo_energies, this_rsp.ebounds, this_rsp.matrix) for this_rsp in self._out_rsp])
 
-                rsp2 = FITSFile(fits_extensions=extensions)
+            rsp2 = FITSFile(fits_extensions=extensions)
 
-                rsp2.writeto("%s.rsp" % self._outfile_basename,overwrite=True)
+            rsp2.writeto("%s.rsp" % self._outfile_basename,overwrite=overwrite)
 
 
 
