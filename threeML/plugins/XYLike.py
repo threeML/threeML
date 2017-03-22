5eea4d7f9897128f4638bfe2fd6ce31940460499
giacomov <giacomo.vianello@gmail.com>
2017-03-22 10:14:37 -0700

    Several new functionalities

--- threeML/plugins/XYLike.py
+++ threeML/plugins/XYLike.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from threeML.plugin_prototype import PluginPrototype
@@ -9,14 +10,14 @@ from threeML.plugins.OGIP.likelihood_functions import poisson_log_likelihood_ide
 from threeML.plugins.OGIP.likelihood_functions import chi2
 from threeML.classicMLE.joint_likelihood import JointLikelihood
 from threeML.data_list import DataList
-
+from threeML.classicMLE.goodness_of_fit import GoodnessOfFit
 
 __instrument_name = "n.a."
 
 
 class XYLike(PluginPrototype):
 
-    def __init__(self, name, x, y, yerr=None, poisson_data=False):
+    def __init__(self, name, x, y, yerr=None, poisson_data=False, quiet=False):
 
         nuisance_parameters = {}
 
@@ -38,7 +39,9 @@ class XYLike(PluginPrototype):
 
             assert np.all(self._yerr > 0), "Errors cannot be negative or zero."
 
-            print("Using chi2 statistic with the provided errors.")
+            if not quiet:
+
+                print("Using chi2 statistic with the provided errors.")
 
             self._is_poisson = False
 
@@ -46,22 +49,21 @@ class XYLike(PluginPrototype):
 
         elif not poisson_data:
 
-            # JM: I'm leaving the possibility to do an unweighted fit... we can remove if this is a
-            #     a bad idea
-
-            #assert poisson_data, "You did not provide errors and you did not set poisson_error to True."
-
             self._yerr = np.ones_like(self._y)
 
             self._is_poisson = False
 
             self._has_errors = False
 
-            print("Using unweighted chi2 statistic.")
+            if not quiet:
+
+                print("Using unweighted chi2 statistic.")
 
         else:
 
-            print("Using Poisson log-likelihood")
+            if not quiet:
+
+                print("Using Poisson log-likelihood")
 
             self._is_poisson = True
             self._yerr = None
@@ -69,6 +71,11 @@ class XYLike(PluginPrototype):
 
         # This will keep track of the simulated datasets we generate
         self._n_simulated_datasets = 0
+
+        # This will contain the JointLikelihood object after a call to .fit()
+        self._joint_like_obj = None
+
+        self._likelihood_model = None
 
     @classmethod
     def from_function(cls, name, function, x, yerr):
@@ -85,6 +92,122 @@ class XYLike(PluginPrototype):
 
         return xyl_gen.get_simulated_dataset(name)
 
+    @classmethod
+    def from_dataframe(cls, name, dataframe, x_column='x', y_column='y', err_column='yerr', poisson=False):
+        """
+        Generate a XYLike instance from a Pandas.DataFrame instance
+
+        :param name: the name for the XYLike instance
+        :param dataframe: the input data frame
+        :param x_column: name of the column to be used as x (default: 'x')
+        :param y_column: name of the column to be used as y (default: 'y')
+        :param err_column: name of the column to be used as error on y (default: 'yerr')
+        :param poisson: if True, then the err_column is ignored and data are treated as Poisson distributed
+        :return: a XYLike instance
+        """
+
+        x = dataframe[x_column]
+        y = dataframe[y_column]
+
+        if poisson is False:
+
+            yerr = dataframe[err_column]
+
+            if np.all(yerr == -99):
+
+                # This is a dataframe generate with the to_dataframe method, which uses -99 to indicate that the
+                # data are Poisson
+                return cls(name, x=x, y=y, poisson=True)
+
+            else:
+
+                # A dataset with errors
+
+                return cls(name, x=x, y=y, yerr=yerr)
+
+        else:
+
+            return cls(name, x=x, y=y, poisson=True)
+
+    @classmethod
+    def from_text_file(cls, name, filename):
+        """
+        Instance the plugin starting from a text file generated with the .to_txt() method. Note that a more general
+        way of creating a XYLike instance from a text file is to read the file using pandas.DataFrame.from_csv, and
+        then use the .from_dataframe method of the XYLike plugin:
+
+        > df = pd.DataFrame.from_csv(filename, ...)
+        > xyl = XYLike.from_dataframe("my instance", df)
+
+        :param name: the name for the new instance
+        :param filename: path to the file
+        :return:
+        """
+
+        df = pd.DataFrame.from_csv(filename, sep=" ")
+
+        return cls.from_dataframe(name, df)
+
+    def to_dataframe(self):
+        """
+        Returns a pandas.DataFrame instance with the data in the 'x', 'y', and 'yerr' column. If the data are Poisson,
+        the yerr column will be -99 for every entry
+
+        :return: a pandas.DataFrame instance
+        """
+
+        x_series = pd.Series.from_array(self.x, name='x')
+        y_series = pd.Series.from_array(self.y, name='y')
+
+        if self._is_poisson:
+
+            # Since DataFrame does not support metadata, there is no way to save the information that the data
+            # are Poisson distributed. We use instead a value of -99 for the error, to indicate that the data
+            # are Poisson
+
+            yerr_series = pd.Series.from_array(np.zeros_like(self.x) * (-99), name='yerr')
+
+        else:
+
+            yerr_series = pd.Series.from_array(self.yerr, name='yerr')
+
+        df = pd.concat((x_series, y_series, yerr_series), axis=1)
+
+        return df
+
+    def to_txt(self, filename):
+        """
+        Save the dataset in a text file. You can read the content back in a dataframe using:
+
+        > df = pandas.DataFrame.from_csv(filename, sep=' ')
+
+        and recreate the XYLike instance as:
+
+        > xyl = XYLike.from_dataframe(df)
+
+        :param filename: Name of the output file
+        :return: none
+        """
+
+        df = self.to_dataframe()  # type: pd.DataFrame
+
+        df.to_csv(filename, sep=" ")
+
+    def to_csv(self, *args, **kwargs):
+        """
+        Save the data in a comma-separated-values file (CSV) file. All keywords arguments are passed to the
+        pandas.DataFrame.to_csv method (see the documentation from pandas for all possibilities). This gives a very
+        high control on the format of the output
+
+        All arguments are forwarded to pandas.DataFrame.to_csv
+
+        :return: none
+        """
+
+        df = self.to_dataframe()
+
+        df.to_csv(**kwargs)
+
     @property
     def x(self):
 
@@ -96,7 +219,7 @@ class XYLike(PluginPrototype):
         return self._y
 
     @property
-    def y_err(self):
+    def yerr(self):
 
         return self._yerr
 
@@ -186,19 +309,27 @@ class XYLike(PluginPrototype):
 
             new_y = np.random.normal(expectation, self._yerr)
 
-        return type(self)(new_name, self._x, new_y, yerr=self._yerr, poisson_data=self._is_poisson)
+        return type(self)(new_name, self._x, new_y, yerr=self._yerr, poisson_data=self._is_poisson, quiet=True)
 
     def plot(self, x_label='x', y_label='y', x_scale='linear', y_scale='linear'):
 
         fig, sub = plt.subplots(1,1)
 
-        sub.errorbar(self.x, self.y, yerr=self.y_err, fmt='.')
+        sub.errorbar(self.x, self.y, yerr=self.yerr, fmt='.')
 
         sub.set_xscale(x_scale)
         sub.set_yscale(y_scale)
 
         sub.set_xlabel(x_label)
         sub.set_ylabel(y_label)
+
+        if self._likelihood_model is not None:
+
+            flux = self._likelihood_model.get_total_flux(self.x)
+
+            sub.plot(self.x, flux, '--', label='model')
+
+            sub.legend(loc=0)
 
         return fig
 
@@ -234,9 +365,24 @@ class XYLike(PluginPrototype):
 
         model = Model(pts)
 
-        jl = JointLikelihood(model, DataList(self), verbose=False)
+        self.set_model(model)
 
-        jl.set_minimizer(minimizer)
+        self._joint_like_obj = JointLikelihood(model, DataList(self), verbose=False)
 
-        return jl.fit()
+        self._joint_like_obj.set_minimizer(minimizer)
+
+        return self._joint_like_obj.fit()
+
+    def goodness_of_fit(self, n_iterations=1000, continue_of_failure=False):
+        """
+        Returns the goodness of fit of the performed fit
+
+        :param n_iterations: number of Monte Carlo simulations to generate
+        :param continue_of_failure: whether to continue or not if a fit fails (default: False)
+        :return: tuple (goodness of fit, frame with all results, frame with all likelihood values)
+        """
+
+        g = GoodnessOfFit(self._joint_like_obj)
+
+        return g.by_mc(n_iterations, continue_of_failure)
 
