70137f255dd906b19cef620ca6f372c613dd7e77
grburgess <jmichaelburgess@gmail.com>
2017-04-30 09:38:10 +0200

    work on reading

--- threeML/plugins/spectrum/binned_spectrum_set.py
+++ threeML/plugins/spectrum/binned_spectrum_set.py
@@ -5,6 +5,7 @@ from threeML.utils.time_interval import TimeIntervalSet
 from threeML.plugins.OGIP.event_polynomial import fit_global_and_determine_optimum_grade, polyfit
 from threeML.exceptions.custom_exceptions import custom_warnings
 from threeML.io.progress_bar import progress_bar
+from threeML.plugins.spectrum.pha_spectrum import PHASpectrum
 import astropy.io.fits as fits
 
 
@@ -22,7 +23,8 @@ class BinnedSpectrumSet(object):
         self._binned_spectrum_list = binned_spectrum_list
         self._reference_time = reference_time
 
-        self._time_intervals = time_intervals #type: TimeIntervalSet
+        self._time_intervals = time_intervals  - reference_time#type: TimeIntervalSet
+
 
     @property
     def reference_time(self):
@@ -151,7 +153,7 @@ class BinnedSpectrumSet(object):
 
 
     @classmethod
-    def from_pha2_fits(cls, pha2_file):
+    def from_pha2_fits(cls, pha2_file,*rsp_files):
 
         with fits.open(pha2_file) as f:
 
@@ -191,9 +193,53 @@ class BinnedSpectrumSet(object):
                 raise RuntimeError("This appears to be a PHA I and not PHA II file")
 
 
+            assert len(rsp_files) == 1 or len(rsp_files) == num_spectra, 'The number of RSPs input does not math the number of spectra in the PHAII file'
 
-            list_of_binned_spectra =[ BinnedSpectrum.from_fits_file('%s{%d}'%(pha2_file,spectrum_number),
-                                                                    file_type='observed') for spectrum_number in range(1, num_spectra+1)]
+            # if one rsp file is used for all spectra, then we create
+            # a proper length array to account for that
+
+            if len(rsp_files) < num_spectra:
+
+                rsp_files = [rsp_files[0]] * num_spectra
+
+
+
+            list_of_binned_spectra =[ PHASpectrum(pha2_file,
+                                                  spectrum_number=spectrum_number,
+                                                  file_type='observed',
+                                                  rsp_file=rsp_files[spectrum_number-1]) for spectrum_number in range(1, num_spectra+1)]
+
+
+
+            # now get the time intervals
+
+            start_times = data.field('TIME')
+            stop_times = data.field('ENDTIME')
+
+            time_intervals = TimeIntervalSet.from_starts_and_stops(start_times, stop_times)
+
+            reference_time = 0
+
+            # see if there is a reference time in the file
+
+            if 'TRIGTIME' in spectrum.header:
+
+                reference_time = spectrum.header['TRIGTIME']
+
+
+            for t_number in range(spectrum.header['TFIELDS']):
+
+                if 'TZERO%d' %t_number in spectrum.header:
+
+                    reference_time = spectrum.header['TZERO%d' %t_number]
+
+
+            return cls( list_of_binned_spectra, reference_time=reference_time, time_intervals=time_intervals)
+
+
+
+
+
 
 
 
