13d141e7f22d76b451ce518c6017c591b9775ef9
giacomov <giacomo.vianello@gmail.com>
2017-05-22 17:15:14 -0700

    Files to generate a Conda distribution of 3ML and astromodels

---
+++ conda-dist/recipes/astromodels/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install -v .
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
