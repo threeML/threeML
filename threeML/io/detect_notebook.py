59478c38be36dc91a141a1dd8529d786db08db77
Travis CI <travis@travis-ci.org>
2017-06-18 02:45:48 -0700

    Better way of detecting if we are running in a notebook

---
+++ threeML/io/detect_notebook.py
@@ -0,0 +1,26 @@
+# Functions to detect whether we are running inside a notebook or not
+
+from IPython import get_ipython
+
+
+def is_inside_notebook():
+
+    ip = get_ipython()
+
+    if ip is None:
+
+        # This happens if we are running in a python session, not a IPython one (for example in a script)
+        return False
+
+    else:
+
+        # We are running in a IPython session, either in a console or in a notebook
+        if ip.has_trait('kernel'):
+
+            # We are in a notebook
+            return True
+
+        else:
+
+            # We are not in a notebook
+            return False
