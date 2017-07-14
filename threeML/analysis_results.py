c81ad905d9587a02f08d51f43d7b872941903e60
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-07-14 14:49:33 +0200

    RuntimeError should be raised, not just constructed (typo leading to a problem)

--- threeML/analysis_results.py
+++ threeML/analysis_results.py
@@ -863,7 +863,7 @@ class BayesianResults(_AnalysisResults):
 
 
         if not has_chainconsumer:
-            RuntimeError("You must have chainconsumer installed to use this function: pip install chainconsumer")
+            raise RuntimeError("You must have chainconsumer installed to use this function: pip install chainconsumer")
 
         # these are the keywords for the plot command
 
@@ -937,7 +937,7 @@ class BayesianResults(_AnalysisResults):
 
 
         if not has_chainconsumer:
-            RuntimeError("You must have chainconsumer installed to use this function")
+            raise RuntimeError("You must have chainconsumer installed to use this function")
 
         cc = chainconsumer.ChainConsumer()
 
