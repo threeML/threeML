d2d92a1825e15dceca690e077a1deb422d630fa7
grburgess <jmichaelburgess@gmail.com>
2017-07-16 16:58:21 +0200

    added some checks

--- threeML/io/plotting/data_residual_plot.py
+++ threeML/io/plotting/data_residual_plot.py
@@ -56,13 +56,11 @@ class ResidualPlot(object):
 
                 except(AttributeError):
 
-                    assert len(model_subplot) == 1, 'The axes you have supplied is invalid'
-
                     # the user supplied a list of axes
 
                     self._data_axis = model_subplot[0]
 
-                    self._fig = self._data_axis.get_figure()
+            self._fig = self._data_axis.get_figure()
 
 
 
