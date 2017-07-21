ca7a210d03c06d388edfc891b24f69d0503803aa
grburgess <jmichaelburgess@gmail.com>
2017-07-21 11:36:17 +0200

    added test with generators

---
+++ threeML/test/test_spectrumlike.py
@@ -0,0 +1,94 @@
+from threeML.plugins.SpectrumLike import SpectrumLike
+from threeML.plugins.DispersionSpectrumLike import DispersionSpectrumLike
+from threeML.io.package_data import get_path_of_data_file
+from threeML.plugins.OGIP.response import OGIPResponse
+from threeML import JointLikelihood, DataList
+
+from astromodels import Blackbody, Powerlaw, Model, PointSource
+
+import numpy as np
+
+
+
+
+def test_spectrumlike_fit():
+
+    energies = np.logspace(1, 3, 51)
+
+    low_edge = energies[:-1]
+    high_edge = energies[1:]
+
+    sim_K = 1E-1
+    sim_kT = 20.
+
+    # get a blackbody source function
+    source_function = Blackbody(K=sim_K, kT=sim_kT)
+
+    # power law background function
+    background_function = Powerlaw(K=1, index=-1.5, piv=100.)
+
+    spectrum_generator = SpectrumLike.from_function('fake',
+                                                    source_function=source_function,
+                                                    background_function=background_function,
+                                                    energy_min=low_edge,
+                                                    energy_max=high_edge)
+
+    bb = Blackbody()
+
+    pts = PointSource('mysource', 0, 0, spectral_shape=bb)
+
+    model = Model(pts)
+
+    # MLE fitting
+
+    jl = JointLikelihood(model, DataList(spectrum_generator))
+
+    result = jl.fit()
+
+    K_variates = jl.results.get_variates('mysource.spectrum.main.Blackbody.K')
+
+    kT_variates = jl.results.get_variates('mysource.spectrum.main.Blackbody.kT')
+
+    assert np.all(np.isclose([K_variates.mean(), kT_variates.mean()], [sim_K, sim_kT], atol=1 ))
+
+def test_dispersionspectrumlike_fit():
+
+
+
+    response = OGIPResponse(get_path_of_data_file('datasets/ogip_powerlaw.rsp'))
+
+    sim_K = 1E-1
+    sim_kT = 20.
+
+    # get a blackbody source function
+    source_function = Blackbody(K=sim_K, kT=sim_kT)
+
+    # power law background function
+    background_function = Powerlaw(K=1, index=-1.5, piv=100.)
+
+    spectrum_generator = DispersionSpectrumLike.from_function('test', source_function=source_function,
+                                                                    response=response,
+                                                                         background_function=background_function)
+
+
+    bb = Blackbody()
+
+    pts = PointSource('mysource', 0, 0, spectral_shape=bb)
+
+    model = Model(pts)
+
+    # MLE fitting
+
+    jl = JointLikelihood(model, DataList(spectrum_generator))
+
+    result = jl.fit()
+
+    K_variates = jl.results.get_variates('mysource.spectrum.main.Blackbody.K')
+
+    kT_variates = jl.results.get_variates('mysource.spectrum.main.Blackbody.kT')
+
+    assert np.all(np.isclose([K_variates.mean(), kT_variates.mean()], [sim_K, sim_kT], atol=1))
+
+
+
+
