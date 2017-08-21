90f1371ed1eb8d6b634976212b70341304891dcf
Giacomo Vianello <giacomo.vianello@gmail.com>
2017-08-21 13:17:33 -0700

    New build system for conda [ci skip]

--- conda-dist/build_inside_container.sh
+++ conda-dist/build_inside_container.sh
@@ -3,6 +3,7 @@ wget -q https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
+# Install conda build system
 conda install -y conda-build
 conda install -y anaconda-client
 conda update -y conda conda-build anaconda-client
