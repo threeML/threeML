704bbef6b1df22a91bd814b27644d02cbcc6a2d4
Giacomo Vianello <giacomo.vianello@gmail.com>
2017-09-03 08:27:09 -0700

    Improving conda release mechanism

--- conda-dist/make_packages.sh
+++ conda-dist/make_packages.sh
@@ -4,6 +4,14 @@ if [ "$(uname)" == "Linux" ]; then
     
     docker run -v `pwd`/../:/3ML --rm -it quay.io/pypa/manylinux1_x86_64 bash -c "source /3ML/conda-dist/build_inside_container.sh"
 
+else
+    
+    # OS-x
+    
+    cd recipes
+    source activate
+    conda build threeml
+
 fi
 
 
