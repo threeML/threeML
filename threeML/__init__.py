5f258bbda50789f79445ab07a7ac501b523b08fc
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-01-15 20:04:03 +0100

    fix imports

--- threeML/__init__.py
+++ threeML/__init__.py
@@ -199,8 +199,10 @@ from data_list import DataList
 
 
 
-from .io.plotting import *
-
+from threeML.io.plotting.model_plot import plot_point_source_spectra
+from threeML.io.plotting.light_curve_plots import plot_tte_lightcurve
+from threeML.io.plotting.post_process_data_plots import display_spectrum_model_counts, display_histogram_fit, \
+    display_photometry_model_magnitudes
 
 # Import the joint likelihood set
 from .classicMLE.joint_likelihood_set import JointLikelihoodSet, JointLikelihoodSetAnalyzer
