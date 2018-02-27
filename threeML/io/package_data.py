bb8c36dbefe9c7d4f3c521b77e9f30450b3c6217
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-02-26 17:31:52 -0800

    New function to get the path of the user dir

--- threeML/io/package_data.py
+++ threeML/io/package_data.py
@@ -1,4 +1,5 @@
 import pkg_resources
+import os
 
 
 def get_path_of_data_file(data_file):
@@ -7,8 +8,17 @@ def get_path_of_data_file(data_file):
     return file_path
 
 
-
 def get_path_of_data_dir():
     file_path = pkg_resources.resource_filename("threeML", 'data')
 
     return file_path
+
+
+def get_path_of_user_dir():
+    """
+    Returns the path of the directory containing the user data (~/.threeML)
+
+    :return: an absolute path
+    """
+
+    return os.path.abspath(os.path.expanduser('~/.threeML'))
