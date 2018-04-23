771ada00af163e5ebfd531b6b813eca755b842a8
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-04-23 15:43:50 -0700

    Corrected a problem with the numerical computation of the covariance matrix

--- threeML/minimizer/minimization.py
+++ threeML/minimizer/minimization.py
@@ -732,8 +732,8 @@ class Minimizer(object):
         :return: the covariance matrix
         """
 
-        minima = map(lambda parameter:parameter.min_value, self.parameters.values())
-        maxima = map(lambda parameter: parameter.max_value, self.parameters.values())
+        minima = map(lambda parameter:parameter._get_internal_min_value(), self.parameters.values())
+        maxima = map(lambda parameter: parameter._get_internal_max_value(), self.parameters.values())
 
         # Check whether some of the minima or of the maxima are None. If they are, set them
         # to a value 1000 times smaller or larger respectively than the best fit.
@@ -1218,6 +1218,18 @@ except ImportError:
 else:
 
     _minimizers["PAGMO"] = PAGMOMinimizer
+
+try:
+
+    from threeML.minimizer.scipy_minimizer import ScipyMinimizer
+
+except ImportError:
+
+    custom_warnings.warn("Scipy minimizer is not available", ImportWarning)
+
+else:
+
+    _minimizers["SCIPY"] = ScipyMinimizer
 
 # Check that we have at least one minimizer available
 
