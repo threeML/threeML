44c6430589c2914ac000b8a8cbcfa6c39f6be2a8
J. Michael Burgess <jmichaelburgess@gmail.com>
2020-03-09 00:06:36 +0100

    time series

--- threeML/utils/time_series/event_list.py
+++ threeML/utils/time_series/event_list.py
@@ -1,3 +1,8 @@
+from __future__ import print_function
+from __future__ import division
+from builtins import zip
+from builtins import range
+from past.utils import old_div
 __author__ = 'grburgess'
 
 import collections
@@ -271,7 +276,7 @@ class EventList(TimeSeries):
 
                 # capture the bkg *rate*
 
-                bkg.append(tmpbkg / this_width)
+                bkg.append(old_div(tmpbkg, this_width))
 
         else:
 
@@ -327,7 +332,7 @@ class EventList(TimeSeries):
 
     def count_per_channel_over_interval(self, start, stop):
 
-        channels = range(self._first_channel, self._n_channels + self._first_channel)
+        channels = list(range(self._first_channel, self._n_channels + self._first_channel))
 
         counts_per_channel = np.zeros(len(channels))
 
@@ -440,7 +445,7 @@ class EventList(TimeSeries):
 
             self._optimal_polynomial_grade = self._user_poly_order
 
-        channels = range(self._first_channel, self._n_channels + self._first_channel)
+        channels = list(range(self._first_channel, self._n_channels + self._first_channel))
 
         polynomials = []
 
@@ -526,7 +531,7 @@ class EventList(TimeSeries):
 
             self._optimal_polynomial_grade = self._user_poly_order
 
-        channels = range(self._first_channel, self._n_channels + self._first_channel)
+        channels = list(range(self._first_channel, self._n_channels + self._first_channel))
 
         # Check whether we are parallelizing or not
 
@@ -972,7 +977,7 @@ class EventListWithLiveTime(EventList):
 
             dt = self._live_time_stops[inside_idx] - self._live_time_starts[inside_idx]
 
-            fraction = (stop - start) / dt
+            fraction = old_div((stop - start), dt)
 
             total_livetime = self._live_time[inside_idx] * fraction
 
@@ -998,7 +1003,7 @@ class EventListWithLiveTime(EventList):
 
             distance_from_next_bin = self._live_time_stops[left_remainder_idx] - start
 
-            fraction = distance_from_next_bin / dt
+            fraction = old_div(distance_from_next_bin, dt)
 
             left_fractional_livetime = self._live_time[left_remainder_idx] * fraction
 
@@ -1012,7 +1017,7 @@ class EventListWithLiveTime(EventList):
 
             distance_from_next_bin = stop - self._live_time_starts[right_remainder_idx]
 
-            fraction = distance_from_next_bin / dt
+            fraction = old_div(distance_from_next_bin, dt)
 
             right_fractional_livetime = self._live_time[right_remainder_idx] * fraction
 
