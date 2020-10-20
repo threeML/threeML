2cdd6df1e414b6a438e409fa1deceafb6c5336b7
ndilalla <niccolo.dilalla@stanford.edu>
2020-10-20 14:16:31 -0700

    Added a flag to skip a couple of tests if ROOT is available.

--- threeML/test/test_joint_likelihood_set.py
+++ threeML/test/test_joint_likelihood_set.py
@@ -2,6 +2,21 @@ from __future__ import print_function
 from threeML import *
 from .conftest import get_grb_model
 
+try:
+
+    import ROOT
+
+except:
+
+    has_root = False
+
+else:
+
+    has_root = True
+
+skip_if_ROOT_is_available = pytest.mark.skipif(
+    has_root, reason="ROOT is available. Skipping incompatible tests."
+)
 
 # Define a dummy function to return always the same model
 def get_model(id):
@@ -19,7 +34,7 @@ def test_joint_likelihood_set(data_list_bn090217206_nai6):
 
     jlset.go(compute_covariance=False)
 
-
+@skip_if_ROOT_is_available
 def test_joint_likelihood_set_parallel(data_list_bn090217206_nai6):
     def get_data(id):
         return data_list_bn090217206_nai6
