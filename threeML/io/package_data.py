349ae104f866df34007663d33166004270a16aa3
grburgess <jmichaelburgess@gmail.com>
2020-12-16 14:17:07 +0100

    add log file finding functions

--- threeML/io/package_data.py
+++ threeML/io/package_data.py
@@ -1,6 +1,6 @@
 import pkg_resources
 import os
-
+from pathlib import Path
 
 def get_path_of_data_file(data_file):
     file_path = pkg_resources.resource_filename("threeML", "data/%s" % data_file)
@@ -22,3 +22,19 @@ def get_path_of_user_dir():
     """
 
     return os.path.abspath(os.path.expanduser("~/.threeML"))
+
+
+def get_path_of_log_dir() -> Path:
+
+    return get_path_of_user_dir() / "log"
+
+
+_log_file_names = ["usr.log", "dev.log"]
+
+def get_path_of_log_file(log_file: str) -> Path:
+    """
+    returns the path of the log files
+    """
+    assert log_file in _log_file_names, f"{log_file} is not on of {_log_file_names}"
+
+    return get_path_of_log_dir() / log_file
