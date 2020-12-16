aa57c490311aeaa6484efd70c0d5f2b084fbde87
grburgess <jmichaelburgess@gmail.com>
2020-12-16 10:39:02 +0100

    switched to pathlib

--- threeML/io/package_data.py
+++ threeML/io/package_data.py
@@ -1,24 +1,24 @@
 import pkg_resources
 import os
+from pathlib import Path
 
-
-def get_path_of_data_file(data_file):
+def get_path_of_data_file(data_file) -> Path:
     file_path = pkg_resources.resource_filename("threeML", "data/%s" % data_file)
 
-    return file_path
+    return Path(file_path)
 
 
-def get_path_of_data_dir():
+def get_path_of_data_dir() -> Path:
     file_path = pkg_resources.resource_filename("threeML", "data")
 
-    return file_path
+    return Path(file_path)
 
 
-def get_path_of_user_dir():
+def get_path_of_user_dir() -> Path:
     """
     Returns the path of the directory containing the user data (~/.threeML)
 
     :return: an absolute path
     """
 
-    return os.path.abspath(os.path.expanduser("~/.threeML"))
+    return Path("~/.threeML").expanduser()
