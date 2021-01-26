0c54cc8d29cd6fd8da94f4a6566ab1574a065dd6
grburgess <jmichaelburgess@gmail.com>
2021-01-26 15:32:41 +0100

    more to config

--- threeML/config/config_structure.py
+++ threeML/config/config_structure.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from enum import Flag
+from enum import Enum, Flag
 from typing import Any, Dict, List, Optional
 
 from omegaconf import II, MISSING, SI, OmegaConf
@@ -45,3 +45,54 @@ class PublicDataServer:
     public_ftp_Location: Optional[str] = None
     public_http_Location: str = MISSING
     query_form: Optional[str] = None
+
+
+@dataclass(frozen=True)
+class CatalogServer:
+    url: str = MISSING
+
+
+@dataclass
+class InstrumentCatalogs:
+    catalogs: Dict[str, CatalogServer]
+
+
+@dataclass
+class BinnedSpectrumPlot:
+    data_cmap: str = Set1
+    model_cmap: str = Set1
+    step: bool = False
+
+
+@dataclass
+class LightCurve:
+    light_curve_color: str = "#34495E"
+    selection_color: str = "#85929E"
+    background_color: str = "#C0392B"
+    background_selection_color: str = "#E74C3C"
+
+
+class Sampler(Enum):
+    emcee = 1
+    pymultinest = 2
+    zeus = 3
+    dynesty = 4
+    ultranest = 5
+
+
+_sampler_default = (
+
+
+
+)
+
+    
+class Optimizer(Enum):
+    minuit = 1
+    
+    
+
+
+@dataclass
+class BayesianDefault:
+    default_sampler: Sampler.emcee
