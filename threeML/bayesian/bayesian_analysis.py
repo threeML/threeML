7859e4e40d366bd78a83c91dc64cc0808787fb0c
Björn Biltzinger <bjoernbiltzinger@gmx.de>
2021-05-07 23:23:15 +0200

    small bug fix in restore_median_fit

--- threeML/bayesian/bayesian_analysis.py
+++ threeML/bayesian/bayesian_analysis.py
@@ -366,4 +366,4 @@ class BayesianAnalysis(object):
         Sets the model parameters to the mean of the marginal distributions
         """
 
-        self._sampler.restore_median_fit
+        self._sampler.restore_median_fit()
