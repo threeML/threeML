527305c2405ba02af48c3886bd5da79706aac5c6
Xavier Boluna <xboluna@gmail.com>
2021-06-14 15:11:26 -0700

    test script

---
+++ threeML/utils/data_builders/fermi/test.py
@@ -0,0 +1,19 @@
+from lat_transient_builder import TransientLATDataBuilder
+
+lt = TransientLATDataBuilder(triggername = 'bn080916009',
+        outfile = 'analysis',
+        roi = 5.,
+        tstarts = '0.',
+        tstops = '1528.75',
+        irf = 'p8_transient010e',
+        galactic_model = 'template (fixed norm.)',
+        particle_model = 'isotr template',
+        zmax = 105.,
+        emin = 65.,
+        emax = 100000.,
+        ra = 119.88999939 ,
+        dec = -56.7000007629 ,
+        liketype = 'unbinned')#bin_file = ''
+
+lt.run()
+import pdb;pdb.set_trace()
