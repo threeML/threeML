cbe4c1ab0030bbdfc2227718a805b50a3e911cb2
Tobias Preis <tobias.preis@uibk.ac.at>
2025-10-15 11:25:54 +0200

    version independent setup

--- threeML/bayesian/dynesty_sampler.py
+++ threeML/bayesian/dynesty_sampler.py
@@ -41,95 +41,14 @@ class DynestyNestedSampler(UnitCubeSampler):
 
     def setup(
         self,
-        n_live_points=500,
-        bound="multi",
-        sample="auto",
-        periodic=None,
-        reflective=None,
-        update_interval=None,
-        first_update=None,
-        rstate=None,
-        queue_size=None,
-        pool=None,
-        use_pool=None,
-        live_points=None,
-        logl_args=None,
-        logl_kwargs=None,
-        ptform_args=None,
-        ptform_kwargs=None,
-        enlarge=None,
-        bootstrap=None,
-        walks=None,
-        facc=0.5,
-        slices=None,
-        ncdim=None,
-        blob=False,
-        save_evaluation_history=False,
-        history_filename=None,
         **kwargs,
     ):
         """
         Setup the Dynesty nested sampler.
 
-        :param n_live_points: Number of live points used in the nested sampling run.
-            Defaults to ``500``.
-        :type n_live_points: int
-        :param bound: Method for bounding the live points. Options include
-            ``"none"``, ``"single"``, ``"multi"``, or ``"balls"``.
-            Defaults to ``"multi"``.
-        :type bound: str
-        :param sample: Sampling method used for proposing new points. Options include
-            ``"auto"``, ``"uniform"``, ``"rwalk"``, ``"slice"``, or ``"rslice"``.
-            Defaults to ``"auto"``.
-        :type sample: str
-        :param periodic: Indices of parameters with periodic boundary conditions.
-        :type periodic: list[int] or None
-        :param reflective: Indices of parameters with reflective boundary conditions.
-        :type reflective: list[int] or None
-        :param update_interval: Interval (in iterations)
-        :type update_interval: int or float or None
-        :param first_update: Initial update behavior
-        :type first_update: tuple or dict or None
-        :param rstate: Random number generator for reproducibility.
-        :type rstate: numpy.random.Generator or numpy.random.RandomState or None
-        :param queue_size: Number of threads or processes for parallel sampling.
-        :type queue_size: int or None
-        :param pool: Multiprocessing pool for parallel likelihood evaluations.
-        :type pool: multiprocessing.Pool or None
-        :param use_pool: use the pool
-        :type use_pool: dict or None
-        :param live_points: Initial live points for starting the nested sampling run.
-        :type live_points: array-like or None
-        :param logl_args: Positional arguments for the log-likelihood function.
-        :type logl_args: tuple or None
-        :param logl_kwargs: Keyword arguments for the log-likelihood function.
-        :type logl_kwargs: dict or None
-        :param ptform_args: Positional arguments for the prior transform function.
-        :type ptform_args: tuple or None
-        :param ptform_kwargs: Keyword arguments for the prior transform function.
-        :type ptform_kwargs: dict or None
-        :param enlarge: Factor by which to enlarge the bounding ellipsoids.
-        :type enlarge: float or None
-        :param bootstrap: Number of bootstrap resamples for bounding ellipsoids.
-        :type bootstrap: int or None
-        :param walks: Number of random-walk steps used in certain sampling methods.
-        :type walks: int or None
-        :param facc: Target acceptance fraction for slice sampling.
-            Defaults to ``0.5``.
-        :type facc: float
-        :param slices: Number of slices used in the slice sampling method.
-        :type slices: int or None
-        :param ncdim: Dimensionality of coordinate transforms (if applicable).
-        :type ncdim: int or None
-        :param blob: Whether to store additional data for each likelihood evaluation.
-            Defaults to ``False``.
-        :type blob: bool
-        :param save_evaluation_history: Save likelihood evaluation history to file.
-            Defaults to ``False``.
-        :type save_evaluation_history: bool
-        :param history_filename: Path to the file where evaluation history is saved.
-        :type history_filename: str or None
-        :param kwargs: Additional keyword arguments.
+        :param kwargs: Additional keyword arguments - must be same name and type as
+            paramters in constructor of the dynesty.NestedSampler class.
+            Defaults to the values used by dynesty.
         :type kwargs: dict
         """
         log.debug("Setup dynesty sampler")
@@ -137,10 +56,8 @@ class DynestyNestedSampler(UnitCubeSampler):
         self._sampler_kwargs = {}
 
         self._kwargs = locals().copy()
-        for k in ["self", "n_live_points"]:
+        for k in ["self"]:
             self._kwargs.pop(k)
-
-        self._kwargs["nlive"] = n_live_points
         kwargs = self._kwargs.get("kwargs")
         self._kwargs.pop("kwargs")
         self._kwargs.update(kwargs)
@@ -258,102 +175,21 @@ class DynestyDynamicSampler(UnitCubeSampler):
 
     def setup(
         self,
-        nlive_init=500,
-        bound="multi",
-        sample="auto",
-        periodic=None,
-        reflective=None,
-        update_interval=None,
-        first_update=None,
-        rstate=None,
-        queue_size=None,
-        pool=None,
-        use_pool=None,
-        logl_args=None,
-        logl_kwargs=None,
-        ptform_args=None,
-        ptform_kwargs=None,
-        enlarge=None,
-        bootstrap=None,
-        walks=None,
-        facc=0.5,
-        slices=None,
-        ncdim=None,
-        blob=False,
-        history_filename=None,
-        save_evaluation_history=False,
         **kwargs,
     ):
         """
         Setup the Dynesty dynamic nested sampler.
 
-        :param nlive_init: Number of live points used for the initial phase.
-            Defaults to ``500``.
-        :type nlive_init: int
-        :param bound: Method for bounding the live points. Options include
-            ``"none"``, ``"single"``, ``"multi"``, or ``"balls"``.
-            Defaults to ``"multi"``.
-        :type bound: str
-        :param sample: Sampling method used for proposing new points. Options include
-            ``"auto"``, ``"uniform"``, ``"rwalk"``, ``"slice"``, or ``"rslice"``.
-            Defaults to ``"auto"``.
-        :type sample: str
-        :param periodic: Indices of parameters with periodic boundary conditions.
-        :type periodic: list[int] or None
-        :param reflective: Indices of parameters with reflective boundary conditions.
-        :type reflective: list[int] or None
-        :param update_interval: Interval (in iterations) for updating.
-        :type update_interval: int or float or None
-        :param first_update: Initial update behavior.
-        :type first_update: tuple or dict or None
-        :param rstate: Random number generator for reproducibility.
-        :type rstate: numpy.random.Generator or numpy.random.RandomState or None
-        :param queue_size: Number of threads or processes for parallel sampling.
-        :type queue_size: int or None
-        :param pool: Multiprocessing pool for parallel likelihood evaluations.
-        :type pool: multiprocessing.Pool or None
-        :param use_pool: Dict specifying which parts of the algorithm to parallelize.
-            Defaults to disabling prior transform and likelihood parallelization.
-        :type use_pool: dict or None
-        :param logl_args: Positional arguments for the log-likelihood function.
-        :type logl_args: tuple or None
-        :param logl_kwargs: Keyword arguments for the log-likelihood function.
-        :type logl_kwargs: dict or None
-        :param ptform_args: Positional arguments for the prior transform function.
-        :type ptform_args: tuple or None
-        :param ptform_kwargs: Keyword arguments for the prior transform function.
-        :type ptform_kwargs: dict or None
-        :param enlarge: Factor by which to enlarge the bounding ellipsoids.
-        :type enlarge: float or None
-        :param bootstrap: Number of bootstrap resamples for bounding ellipsoids.
-        :type bootstrap: int or None
-        :param walks: Number of random-walk steps used in certain sampling methods.
-        :type walks: int or None
-        :param facc: Target acceptance fraction for slice sampling.
-            Defaults to ``0.5``.
-        :type facc: float
-        :param slices: Number of slices used in the slice sampling method.
-        :type slices: int or None
-        :param ncdim: Dimensionality of coordinate transforms (if applicable).
-        :type ncdim: int or None
-        :param blob: Store additional data for each likelihood evaluation.
-            Defaults to ``False``.
-        :type blob: bool
-        :param history_filename: Path to the file where evaluation history is saved.
-        :type history_filename: str or None
-        :param save_evaluation_history: Save likelihood evaluation history to file.
-            Defaults to ``False``.
-        :type save_evaluation_history: bool
-        :param kwargs: Additional keyword arguments.
+        :param kwargs: Additional keyword arguments - must be same name and type as
+            paramters in constructor of the dynesty.DynamicNestedSampler class.
+            Defaults to the values used by dynesty.
         :type kwargs: dict
         """
 
         log.debug("Setup dynesty dynamic sampler")
         self._sampler_kwargs = {}
-        self._sampler_kwargs["nlive_init"] = nlive_init
-
         self._kwargs = locals().copy()
-        for k in ["self", "nlive_init"]:
+        for k in ["self"]:
             self._kwargs.pop(k)
         kwargs = self._kwargs.get("kwargs")
         self._kwargs.pop("kwargs")
