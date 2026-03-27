9b54ff8b890e83d377865ee0d3b090ce91aa145a
Israel Martinez <imc@umd.edu>
2026-03-27 10:35:50 -0400

    Add tests for new methods add_startup_warning and log_astromodels_startup_warnings

--- threeML/test/test_verbosity.py
+++ threeML/test/test_verbosity.py
@@ -24,6 +24,9 @@ from threeML.io.logging import (
 )
 from threeML.utils.progress_bar import tqdm, trange
 
+from threeML.io.logging import add_startup_warning, log_threeml_startup_warnings
+
+logger = logging.getLogger(__name__)
 
 def test_all_toggles():
     toggle_progress_bars()
@@ -159,3 +162,14 @@ def test_logging_toggles():
     assert astromodels_console_log_handler.level == logging.DEBUG
 
     assert astromodels_usr_log_handler.level == logging.CRITICAL
+
+def test_startup_warnings(caplog):
+
+    msg = "TEST_STARTUP_WARNINGS"
+
+    add_startup_warning(logger, msg)
+
+    with caplog.at_level(logging.WARNING):
+        log_threeml_startup_warnings(logger)
+
+    assert msg in caplog.text
