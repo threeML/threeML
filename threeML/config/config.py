8a13efb4781562ca7d5416ac8794ddc5d2b59e5f
Tobias Preis <tobias.preis@uibk.ac.at>
2026-06-27 09:49:45 +0200

    add: raise warning if startup_warnings key is supplied

--- threeML/config/config.py
+++ threeML/config/config.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import warnings
 
 from omegaconf import OmegaConf
 from .config_structure import Config
@@ -25,5 +26,12 @@ def get_path_of_user_config() -> Path:
 
 for user_config_file in get_path_of_user_config().glob("*.yml"):
     _partial_conf = OmegaConf.load(user_config_file)
+    if "logging" in _partial_conf.keys():
+        if "startup_warnings" in _partial_conf["logging"].keys():
+            warnings.warn(
+                f"You've provided 'logging.startup_warnings' in {user_config_file}. "
+                + "This is deprecated since v2.6.0 - will ignore it"
+            )
+            del _partial_conf.logging.startup_warnings
 
     threeML_config: Config = OmegaConf.merge(threeML_config, _partial_conf)
