89487fcc280e0e2ab53426e249df3622c004d478
Tobias Preis <tobias.preis@uibk.ac.at>
2026-07-09 09:11:22 +0200

    refactor: update user configs as functions

--- threeML/config/config.py
+++ threeML/config/config.py
@@ -24,14 +24,19 @@ def get_path_of_user_config() -> Path:
     return config_path
 
 
-for user_config_file in get_path_of_user_config().glob("*.yml"):
-    _partial_conf = OmegaConf.load(user_config_file)
-    if "logging" in _partial_conf.keys():
-        if "startup_warnings" in _partial_conf["logging"].keys():
-            warnings.warn(
-                f"You've provided 'logging.startup_warnings' in {user_config_file}. "
-                + "This is deprecated since v2.6.0 - will ignore it"
-            )
-            del _partial_conf.logging.startup_warnings
+def update_config_with_user_configs(threeML_config):
+    for user_config_file in get_path_of_user_config().glob("*.yml"):
+        _partial_conf = OmegaConf.load(user_config_file)
+        if "logging" in _partial_conf.keys():
+            if "startup_warnings" in _partial_conf["logging"].keys():
+                warnings.warn(
+                    f"You've provided 'logging.startup_warnings' in {user_config_file}. "
+                    + "This is deprecated since v2.6.0 - will ignore it"
+                )
+                del _partial_conf.logging.startup_warnings
 
-    threeML_config: Config = OmegaConf.merge(threeML_config, _partial_conf)
+        threeML_config: Config = OmegaConf.merge(threeML_config, _partial_conf)
+    return threeML_config
+
+
+threeML_config = update_config_with_user_configs(threeML_config)
